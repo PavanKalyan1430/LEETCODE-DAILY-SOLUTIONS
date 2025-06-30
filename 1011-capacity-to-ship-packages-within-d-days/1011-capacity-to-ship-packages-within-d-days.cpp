397ff06838e059f9d23cc203c1a68a192205ed55
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2025-06-30 09:59:41 +0530

    Time: 13 ms (45.7%), Space: 60.8 MB (6.02%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,54 @@
+int can_store(vector<int> weights, int min_weight){
+    int capacity = 0;
+    int no_of_days= 0;
+
+    for (int i : weights){
+        if ( (capacity + i ) > min_weight){
+            no_of_days +=1;
+            capacity = i;
+        }
+        else capacity+=i;
+    }
+
+    if (capacity > 0) no_of_days +=1;
+
+    return no_of_days;
+}
+
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        
+        int sumy = 0;
+        int maxi = 0;
+
+        for (int i: weights){
+            sumy+= i;
+            maxi = max(maxi, i);}
+
+        int left = maxi;
+        int right = sumy;
+        int no_of_days;
+        int result = INT_MAX;
+        int mid;
+
+        while (left <= right){
+            mid = (left + right ) / 2;
+            no_of_days = can_store(weights, mid);
+
+            if (no_of_days <= days){
+                result = min(result, mid);
+                right = mid-1;
+            }
+            else left = mid+1;
+
+            
+        }
+
+        return result;
+
+
+
+
+    }
+};
