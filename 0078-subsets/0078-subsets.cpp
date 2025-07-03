a5fb16a147d997b54a3663626d6bbb44e578d40d
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2025-07-03 12:27:59 +0530

    Time: 0 ms (100%), Space: 9.6 MB (99.54%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void func(vector<int>& nums, int start, vector<int>& temp, vector<vector<int>>& result){
+        result.push_back(temp);  //Store current subset
+
+        for (int i =start; i<nums.size(); i++){
+            //INCLUDE current element
+            temp.push_back(nums[i]);
+
+            func(nums, i + 1, temp, result);
+
+            //EXCLUDE (backtrack)
+            temp.pop_back();
+        }
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+
+        vector<vector<int>> result;
+        vector<int> temp;
+        func(nums, 0, temp, result);
+        return result;
+    }
+};
