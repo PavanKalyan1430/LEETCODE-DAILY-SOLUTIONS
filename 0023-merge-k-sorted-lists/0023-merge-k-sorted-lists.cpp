ade36e5f0dae9de5017b3d36dd6b817be4f2d53e
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2025-07-03 17:42:15 +0530

    Time: 96 ms (16.26%), Space: 18.5 MB (51.54%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -1,48 +1,56 @@
-ListNode* merge(ListNode* list1, ListNode* list2){
-    // ListNode* dummy = new ListNode(-1);
-    // ListNode*
-        ListNode* dummy = new ListNode(-1);
-        ListNode* start = dummy;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 
-        while(list1 and list2){
-            if (list1->val <= list2->val){
-                start->next = list1;
-                list1 = list1->next;
-            }
+ ListNode* merge(ListNode* l1 , ListNode* l2){
 
-            else {
-                start->next = list2;
-                list2 = list2->next;
-            }
+     ListNode* dummy = new ListNode(-1);
+     ListNode* start = dummy;
 
-            start = start->next;
+     while(l1 and l2){
+        if (l1->val <= l2->val){
+            start->next = l1;
+            l1 = l1->next;
         }
 
-        if (!list1) start->next = list2;
-        else start->next = list1;
+        else {
+            start->next = l2;
+            l2 = l2->next;
+        }
 
-    return dummy->next;
-}
+        start = start->next;
+     }
+
+     if (l1) start->next = l1;
+     else start->next = l2;
+
+     return dummy->next;
+    
+
+ }
 
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        
-       ListNode* result ;
 
-       if (lists.size() == 0 ) return NULL;
-       if (lists.size() == 1) return lists[0];
+        if (lists.size() == 0) return NULL;
+         if (lists.size()==1) return lists[0];
+        
+        ListNode* dummy = new ListNode(-1);
+        ListNode* start = dummy;
 
         for (int i=0; i<lists.size()-1; i++){
-            lists[i+1]  = merge(lists[i] , lists[i+1]);
+            lists[i+1] = merge(lists[i], lists[i+1]);
         }
 
-        return lists[lists.size()-1];
-
-
-
-
-
+        return (lists.back());
 
     }
 };
