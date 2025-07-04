b0d89dc6c54760f39a9875f6dfb8f25677d7cb4f
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2025-07-04 10:07:47 +0530

    Time: 3 ms (29.36%), Space: 12.6 MB (9.76%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,32 @@
+void fun(vector <int> nums, vector<vector<int>> &result, vector <int> temp, int start){
+
+       if (temp.size()==nums.size()){
+        result.push_back(temp);
+        return ;
+      }
+
+      for (int i= 0; i<nums.size(); i++){
+
+        if  (find(temp.begin(), temp.end(), nums[i]) != temp.end()) continue;
+        
+            temp.push_back(nums[i]);
+
+            fun(nums, result, temp, i+1);
+
+            temp.pop_back();
+      }
+
+}
+
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+
+        vector<vector<int>> result;
+        vector <int> temp;
+
+         fun(nums, result, temp, 0);
+
+        return result;
+    }
+};
