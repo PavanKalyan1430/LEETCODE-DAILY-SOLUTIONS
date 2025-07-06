8374f65fe6a3a4c590a60f00d1aa5d743858f7ac
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2025-07-06 16:12:36 +0530

    Time: 0 ms (100%), Space: 9.9 MB (81.2%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,26 +1,29 @@
-void fun (vector<vector<int>> &result, vector <int> &temp, vector<int> nums, int i){
-
-
-    if (i >=nums.size()){
-    result.push_back(temp);
-    return;}
-
-    temp.push_back(nums[i]);
-    fun (result, temp, nums, i+1);
-
-    temp.pop_back();
-    fun (result, temp , nums , i+1);
-
-}
-
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> result;
-        vector <int> temp;
-        int start = 0;
-        fun (result , temp , nums, start);
+    void func(vector<int>& nums, int start, vector<int>& temp, vector<vector<int>>& result){
+        if (start == nums.size()){
+            result.push_back(temp);  //Store current subset
+            return;
+        }
 
+
+
+        
+            //INCLUDE current element
+            temp.push_back(nums[start]);
+            func(nums, start + 1, temp, result);
+
+            //EXCLUDE (backtrack)
+            temp.pop_back();
+            func(nums, start+1, temp, result);
+}
+
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+
+        vector<vector<int>> result;
+        vector<int> temp;
+        func(nums, 0, temp, result);
         return result;
     }
 };
