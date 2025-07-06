bd36b5d7593bd070a0c79161fae4ca3b400aad86
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2025-07-06 16:10:28 +0530

    Time: 3 ms (32.75%), Space: 15.2 MB (23.35%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,26 +1,26 @@
-void fun(vector<vector<int>> &result, vector <int> temp,vector<int> nums, int i){
+void fun (vector<vector<int>> &result, vector <int> &temp, vector<int> nums, int i){
 
 
-    if (i >= nums.size() ){
+    if (i >=nums.size()){
     result.push_back(temp);
-    return ;}
+    return;}
 
     temp.push_back(nums[i]);
-
-    fun(result, temp, nums, i+1);  //INCLUDE
+    fun (result, temp, nums, i+1);
 
     temp.pop_back();
-    fun(result, temp, nums, i+1);
+    fun (result, temp , nums , i+1);
 
 }
 
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> result ;
+        vector<vector<int>> result;
         vector <int> temp;
+        int start = 0;
+        fun (result , temp , nums, start);
 
-        fun (result, temp , nums, 0);
         return result;
     }
 };
