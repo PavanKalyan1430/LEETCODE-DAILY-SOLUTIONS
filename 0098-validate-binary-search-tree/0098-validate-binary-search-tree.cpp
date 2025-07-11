9f14704d4168c942932bdfdf1e94e57cc2320e39
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2025-07-11 09:30:47 +0530

    Time: 0 ms (100%), Space: 21.9 MB (75.44%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -1,38 +1,22 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
- 
- bool HelperFunction(TreeNode* root){
+bool HelperFunction(TreeNode* root, long mini, long maxi){
 
-    if (root == NULL) return true;
+    if (!root ) return true;
 
-    cout<<root->val<<" ";
-    if (root->left and root->left->val >= root->val) return false;
+    if(root->val <=  mini or root->val >= maxi) return false;
 
-    if (root->right and root->val >= root->right->val) return false;
-
-
-    return HelperFunction(root->left) &&  HelperFunction(root->right);
-
-
-
+     return HelperFunction(root->left, mini, root->val) && HelperFunction(root->right, root->val, maxi);
 
 
 }
 
 
-
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
-        return HelperFunction(root);
+        long mini = LONG_MIN;
+        long maxi = LONG_MAX;
+
+        return HelperFunction(root, mini, maxi);
+
     }
 };
