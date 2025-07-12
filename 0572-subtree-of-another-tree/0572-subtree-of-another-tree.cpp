d36a3a15e1877723eb9d1819acc5a02397e44315
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2025-07-12 16:10:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -0,0 +1,29 @@
+bool dfs(TreeNode* root, TreeNode* subRoot){
+
+    if (!root and !subRoot ) return true;
+
+    if ( !root or !subRoot ) return false;
+
+    if (root->val != subRoot->val ) return false;
+
+
+    return dfs(root->left , subRoot->left) and dfs(root->right, subRoot->right);
+
+}
+        
+class Solution {
+public:
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        
+        if (!root) return false;
+        
+        return dfs(root->left, subRoot) || dfs(root->right, subRoot);
+
+        isSubtree(root->left, subRoot);
+        isSubtree(root->right, subRoot);
+
+
+
+
+    }
+};
