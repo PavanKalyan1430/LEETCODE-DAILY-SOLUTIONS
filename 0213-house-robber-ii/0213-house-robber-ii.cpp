e0c9e3b891773b4458113cb3d92cb623b221deca
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2025-07-14 19:15:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        if (nums.size() ==1 ) return nums[0];
-        if (nums.size()== 2) return max(nums[0], nums[1]);
+        
+        if (nums.size() == 1) return 0;
 
-        vector<int> dp(nums.size(),0);
+        int n = nums.size();
+        
 
-        vector<int>new_dp(nums.size(), 0);
+        vector<int> DP (n, 0);   
+        vector<int> new_DP (n,0);
 
-        dp[0] = 0;      //not robbig the first house;
-        dp[1] =  nums[1];
+        DP[0] = 0;              // dont rob 1st house
+        DP[1] = nums[1];
 
-        new_dp[0] = nums[0];
-        new_dp[1] = max(nums[0], nums[1]);
+        new_DP[0] = nums[0];           // dont rob last house
+        new_DP[1] = max(nums[0] , nums[1]);
 
-        for (int i=2; i<nums.size(); i++){
-            dp[i] = max(dp[i-1], nums[i] + dp[i-2]);
-        }
-
-        for (int i : dp) cout<<i<<" ";
-
-        cout<<endl;
-
-        for (int i: new_dp) cout<<i<<" ";
 
         for (int i=2; i<nums.size(); i++){
-
-            if (i == nums.size()-1) new_dp[i] = new_dp[i-1];
-
-            else new_dp[i] = max(new_dp[i-1],  nums[i] + new_dp[i-2]);
+            DP[i] = max(DP[i-1], nums[i]+ DP[i-2]);
         }
 
-       
+        for (int i=2; i<nums.size(); i++){   
+            
+            if (i== n-1) new_DP[i] = new_DP[i-1];     // dont rob last house
 
-        return max(dp.back(),new_dp.back());
+            else new_DP[i] = max(new_DP[i-1], nums[i]+ new_DP[i-2]);
+        }
+
+        return max(DP.back(), new_DP.back());
+
+
+
+
+
 
 
     }
