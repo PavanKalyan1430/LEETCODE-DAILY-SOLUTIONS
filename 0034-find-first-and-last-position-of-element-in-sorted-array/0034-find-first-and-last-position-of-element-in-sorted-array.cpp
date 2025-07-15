2ecb09589de02879c5552219c633ea76e2bb924f
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2025-07-15 15:33:32 +0530

    Time: 0 ms (100%), Space: 17.4 MB (86.42%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,60 +1,60 @@
-int first_index(vector<int>& nums, int target){
-        int left = 0;
-        int right =nums.size()-1;
-
-        int fi=-1;
-
-        while(left <= right ){
-            int mid =( left + right ) / 2 ;
-            if (nums[mid] == target ){
-                fi = mid;
-                right = mid-1;
-            }
-
-            else if (nums[mid] > target){
-                right = mid-1;
-            }
-
-            else left = mid +1;
-
-        }
-        return fi;
-}
-
-int last_index(vector<int>& nums, int target){
+int first_occurence (vector<int>& nums, int &target){
 
     int left = 0;
-    int right =nums.size()-1;
-    int li=-1;
+    int right = nums.size()-1;
+    int mid , ans = -1;
 
-        while(left <= right ){
-            int mid =( left + right ) / 2 ;
+    while(left <= right){
+         
+         mid = (left + right )/ 2;
 
-            if (nums[mid] == target ){
-               li = mid;
-               left = mid+1;
+         if (nums[mid] >= target){
+
+            if (nums[mid] == target){
+                ans  = mid;
             }
+            right = mid-1;
+         }
 
-            else if (nums[mid] > target){
-                right = mid-1;
-            }
+         else left = mid+1;
+    }
+    return ans;
 
-            else left = mid +1;
-
-        }
-        return li;
 }
+
+int last_occurence (vector<int>& nums, int &target){
+
+    int left = 0;
+    int right = nums.size()-1;
+    int mid, ans = -1;
+
+    while(left <= right){
+         
+         mid = (left + right )/ 2;
+
+         if (nums[mid] <= target){
+
+            if (nums[mid] == target){
+                ans  = mid;
+            }
+            left = mid+1;
+         }
+
+         else right = mid-1;
+    }
+
+    return ans;
+
+}
+
 
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
+        
+        int f_occur = first_occurence(nums, target);
+        int l_occur = last_occurence(nums, target);
 
-        int fi = first_index(nums,  target);
-        int li = last_index( nums,  target);
-
-        return {fi, li};
-
-
-
+        return {f_occur, l_occur};
     }
 };
