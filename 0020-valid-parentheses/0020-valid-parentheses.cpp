8cb7cc784e61ad7ed9a010d6881273b9e752d3a2
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2025-07-23 09:53:16 +0530

    Time: 0 ms (100%), Space: 8.6 MB (96.49%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string str) {
+        
+        stack <char> s;
+
+        for (auto i : str){
+
+            if (s.empty() or i== '(' or i=='{' or i=='[' ) s.push(i);
+
+            else if ( i == ')' and s.top() == '(') s.pop();
+
+            else if (i == '}' and s.top() == '{') s.pop();
+
+            else if (i == ']' and s.top() == '[') s.pop();
+
+            else s.push(i);
+
+
+        }
+        return s.empty();
+
+
+    }
+};
