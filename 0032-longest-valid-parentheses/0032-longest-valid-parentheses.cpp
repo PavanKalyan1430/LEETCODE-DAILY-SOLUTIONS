c851bf16bebc0007f2d1825785527694bb265efc
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2025-07-23 12:00:43 +0530

    Time: 4 ms (13.66%), Space: 9.7 MB (89.65%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -1,29 +1,42 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        stack <int> stacky;
-        int maxi =0;
-        vector <int> mark(s.size(),0);
-        int count =0;
-        for (int i=0; i<s.size(); i++){
-            if (s[i] == '(') stacky.push(i);
-            else if (s[i] == ')' and !stacky.empty()){
-                int pos = stacky.top();
-                stacky.pop();
-                mark[pos] =1;
-                mark[i] = 1;
-            }  
-        }
-        //for (int i: mark) cout<<i<<" ";
-        for (int i=0; i < mark.size();i++){
-            cout<<i<<" ";
-            if (mark[i]==1){
-                count+=1;
-                maxi = max(maxi, count);
-            }
-            else count=0;
-        }
-    return maxi;
-    }
+        
+        int open =0;
+        int close = 0;
+        int result = 0;
 
+        for (int i =0; i<s.size(); i++){
+
+            if ( s[i] == '(') open+=1;
+            
+            else if  ( s[i]== ')' ) close+=1;
+
+            if ( close > open ){
+                open = 0;
+                close = 0;
+            }
+
+            else if ( open == close) result = max(result , open*2);
+
+        }
+        open = 0;
+        close = 0;
+
+        for (int i= s.size()-1; i>=0; i--){
+
+            if ( s[i] == '(') open+=1;
+            else if (s[i] == ')') close+=1;
+
+            if (open > close){
+                open = 0;
+                close = 0;
+            }
+
+            else if ( open == close) result = max(result , open*2);
+        }
+
+        return result;
+
+    }
 };
