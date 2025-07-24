9c2409eb960487921d6f39ae1cd57a298ba31914
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2025-07-24 09:40:06 +0530

    Time: 4 ms (18.17%), Space: 97.2 MB (86.8%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -3,17 +3,15 @@ public:
     int maxProfit(vector<int>& nums) {
         
         int mini = nums[0];
-        int result = 0;
+        int profit = 0;
 
         for (int i=1; i<nums.size(); i++){
 
             mini = min(mini, nums[i]);
+            profit = max(profit, nums[i]-mini);
 
-            result = max( result, nums[i]-mini);
         }
 
-        return result;
-
-
+        return profit;
     }
 };
