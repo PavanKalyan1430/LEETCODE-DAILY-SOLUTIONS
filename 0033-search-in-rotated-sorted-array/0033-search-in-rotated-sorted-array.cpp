c11493b62f116c5524949ef9f3da4b2abcdb6df2
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2025-07-24 12:29:29 +0530

    Time: 0 ms (100%), Space: 15.1 MB (94.98%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -6,30 +6,27 @@ public:
         int right = nums.size()-1;
         int mid;
 
-        while( left <= right){
+        while ( left <= right){
 
             mid = ( left + right ) / 2;
 
             if (nums[mid] == target) return mid;
 
             else if (nums[left] <= nums[mid]){
-                if (nums[left] <= target and target < nums[mid]){
+                if ( nums[left] <= target and target<=nums[mid]){
                     right = mid-1;
                 }
                 else left = mid+1;
             }
 
-            else{
-                if (nums[mid] < target and target <= nums[right]){
+            else {
+                if (nums[mid] <= target and target <= nums[right]){
                     left = mid+1;
                 }
-                else right = mid-1;
+                else right =  mid-1;
             }
 
         }
-
-        return -1;
-
-
+            return -1;
     }
 };
