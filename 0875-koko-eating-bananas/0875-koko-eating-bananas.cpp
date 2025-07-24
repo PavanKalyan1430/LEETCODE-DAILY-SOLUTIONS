e7d56144eb28a4e33de235426e5bec956d000ee2
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2025-07-24 12:56:29 +0530

    Time: 5 ms (82.16%), Space: 22.9 MB (73.77%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,53 +1,45 @@
-long long can_store(int bananas_per_hour, vector<int> piles) {
+long long can_eat(vector<int> &piles, int banana_per_hour ){
 
-    long long hours = 0;;
+    long long hours = 0;
 
-    for (int i : piles){
-        hours +=  ( (i + bananas_per_hour)- 1) / bananas_per_hour;
+    for ( int i : piles){
+         hours += ( (i + banana_per_hour -1) / banana_per_hour);
     }
 
     return hours;
 
-
 }
-
 
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        int maxi = 0;
-
-        for (int i: piles) maxi = max(maxi, i);
-
+        
         int left = 1;
-        int right = maxi;
+        int right = *max_element(piles.begin(), piles.end());
         int mid;
-        long long    hours;
+
+        long long hours;
         int result = INT_MAX;
 
-        while(left <= right) {
-
+        while ( left <= right){
 
             mid = ( left + right ) / 2;
 
+            hours = can_eat(piles, mid);
 
-            hours = can_store(mid,piles);
-
-            cout<<mid<<"   " << "hours :  "<< hours<<endl;
-
-            if (hours <= h){
-                
+            if ( hours <= h){
                 result = min(result , mid);
                 right = mid-1;
-
             }
 
             else left = mid+1;
 
 
+            
         }
 
         return result;
+
 
     }
 };
