1684b00b6c47361436a491e951203137e35a3724
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2025-07-25 11:25:29 +0530

    Time: 46 ms (39.63%), Space: 53 MB (13.47%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -5,16 +5,17 @@ public:
         int n = amount;
 
         // Step 1: Initialize DP table
-        vector<vector<int>> DP(m + 1, vector<int>(n + 1, 0));
+        vector<vector<int>> DP(m + 1, vector<int>(n + 1, 10001));
 
         // Step 2: Fill the first row (0 coins) with INT_MAX - 1 for all positive amounts
-        for (int j = 1; j <= n; j++) {
-            DP[0][j] = INT_MAX - 1;
-        }
+        // for (int j = 1; j <= n; j++) {
+        //     DP[0][j] = INT_MAX - 1;
+        // }
 
+        DP[0][0]=0;
         // Step 3: Fill the rest of the table
         for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
+            for (int j = 0; j <= n; j++) {
                 if (coins[i - 1] > j) {
                     // Coin can't be included
                     DP[i][j] = DP[i - 1][j];
@@ -25,6 +26,6 @@ public:
             }
         }
 
-        return DP[m][n] == INT_MAX - 1 ? -1 : DP[m][n];
+        return DP[m][n] == 10001 ? -1 : DP[m][n];
     }
 };
