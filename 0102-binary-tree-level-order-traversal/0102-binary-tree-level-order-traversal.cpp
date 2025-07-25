7852f9210e655738f5db1b94a30521a8fc5c87a5
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2025-07-25 16:11:46 +0530

    Time: 0 ms (100%), Space: 17.1 MB (44.29%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -9,39 +9,37 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
- void HelperFunction(TreeNode* root, queue<TreeNode*> q, vector<vector<int>> &result ){
-
-    if (!root) return;
-    q.push(root);
-
-    while(!q.empty()){
-        int size = q.size();
-        vector<int> temp;
-
-        for (int i=0; i<size; i++){
-            TreeNode* curr = q.front();
-            q.pop();
-
-            if (curr->left) q.push(curr->left);
-            if (curr->right) q.push(curr->right);
-
-            temp.push_back(curr->val);
-        }
-
-        result.push_back(temp);
-    }
- }
 class Solution {
 public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-
-        queue<TreeNode*> q;
+    vector<vector<int>> levelOrder(TreeNode* root) {  
+        queue <TreeNode*> q;
         vector<vector<int>> result;
 
+        if (!root) return {};
 
-        HelperFunction(root, q, result);
+        q.push(root);
+
+        while(q.size() > 0){
+            int size = q.size();
+            vector <int> temp;
+
+            for (int i=0 ;i<size; i++){
+                TreeNode* front = q.front();
+                q.pop();
+
+                temp.push_back(front->val);
+
+                if (front->left ) q.push(front->left);
+                if (front->right) q.push(front->right);
+
+
+            }
+            result.push_back(temp);
+
+        }
 
         return result;
+        
+
     }
 };
