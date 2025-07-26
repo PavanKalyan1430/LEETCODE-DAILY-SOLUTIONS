5b2a3bc62cd5d9090785018091713a98a54bc3dc
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2025-07-26 22:12:02 +0530

    Time: 0 ms (100%), Space: 16.9 MB (61.32%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -2,62 +2,64 @@ class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
         
-        int rotten = 0;
-        int fresh = 0;
         int minutes = 0;
+
+        int fresh = 0;
+        int rotten = 0;
+
+        queue<pair<int,int>>q;
 
         int rows = grid.size();
         int columns = grid[0].size();
 
-        queue<pair<int,int>> q; 
 
-        for (int i=0; i<rows ; i++){
-            for (int j= 0 ; j<columns ; j++){
 
-                if (grid[i][j] == 1) fresh+=1;
+         for (int i=0; i<rows; i++){
+            for (int j= 0; j<columns; j++){
+                if ( grid[i][j] == 1){
+                    fresh+=1;
+                }
 
                 else if (grid[i][j] == 2){
-                    rotten+=1;
+                    rotten +=1;
                     q.push({i,j});
                 }
             }
-        }
+         }
 
-        if (fresh == 0) return 0;
-        if (rotten == 0) return -1;
+         if (fresh == 0) return 0;
+         if ( rotten == 0) return -1;
 
-        vector<int> dx = {0,-1,0,1};
-        vector<int> dy = {1,0,-1,0};
+         vector <int> dx = {0, -1, 0, 1};
+         vector <int> dy = {1, 0, -1, 0};
 
-        while (q.size() > 0){
+         while ( q.size() > 0)  {
+
             minutes +=1;
-
             int size = q.size();
 
             for (int i=0; i<size; i++){
-                pair <int,int> point = q.front();
+
+                pair<int, int> point = q.front();
                 q.pop();
 
-                for (int i=0; i<4; i++){
-                    int x = point.first + dx[i];
-                    int y = point.second + dy[i];
+                for (int j=0; j<4; j++){
+                    int x = point.first + dx[j];
+                    int y = point.second + dy[j];
 
-                    if (x >=0 and y>=0 and x<rows and y<columns and grid[x][y] == 1){
-                        q.push({x,y});
+                    if ( x>= 0 and y>=0 and x<rows and y<columns  and grid[x][y] == 1){
                         grid[x][y] = 2;
                         fresh -=1;
+                        q.push({x,y});
+                        if ( fresh == 0) return minutes;
                     }
                 }
 
-                if (fresh == 0) return minutes;
             }
 
-        }
+         }
 
-        if (fresh == 0) return minutes;
-        else return -1;
-            
-        
+         return -1;
 
     }
 };
