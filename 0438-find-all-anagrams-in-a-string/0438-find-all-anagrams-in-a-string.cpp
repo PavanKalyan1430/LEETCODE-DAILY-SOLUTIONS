258dc520bd500ad7cd1f63c543d015dfe5c44299
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2025-09-13 19:47:18 +0530

    Time: 1102 ms (5%), Space: 15.6 MB (16.97%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        
+        int left = 0;
+        unordered_map <char, int> need, have;
+        vector <int> result;
+
+        for (int i=0; i<p.size(); i++){
+            need[p[i]]++;
+        }
+
+        int need_count = need.size();
+        int have_count = 0;
+
+        for (int right = 0; right<s.size(); right++){
+
+            have[s[right]]++;
+
+            if (need.count(s[right]) and need[s[right]] == have[s[right]]) have_count++;
+
+            while ( right- left + 1>= p.size()) {
+
+                
+                if (need_count == have_count and right-left+1 == p.size() ){
+                    result.push_back(left);
+                }
+
+                if (need.count(s[left]) and need[s[left]] == have[s[left]]  ) have_count--;
+
+                have[s[left]]--;
+                if (have[s[left]] == 0) have.erase(s[left]);
+                left++;
+                cout<<  "after drop  "<<need_count <<"  " <<have_count<<endl;
+                cout<<s[left] <<"  " <<s[right]<<endl;
+
+
+
+            }
+        }
+
+        return result;
+
+
+    }
+};
