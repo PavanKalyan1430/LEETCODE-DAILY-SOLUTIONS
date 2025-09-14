69e7f34a1b45a37b7bf0a924c51c17d55a6f6155
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2025-09-14 16:29:02 +0530

    Time: 19 ms (12.12%), Space: 19.8 MB (7.29%) - LeetHub

--- 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
@@ -6,11 +6,10 @@ public:
       unordered_set <char> sety = {'a', 'e', 'i', 'o', 'u'};
 
        for (int i=0; i<s.size(); i++){
-        if (sety.count(tolower(s[i]))) vowel_count +=1;
+        if (sety.count(tolower(s[i]))) return true;
        } 
 
-       if (vowel_count == 0) return false;
-       else  return true;
+        return false;
 
 
 
