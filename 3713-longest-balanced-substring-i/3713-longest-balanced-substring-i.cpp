36e10622ce2a9d66557de2172b6772ed22de55ee
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2025-10-24 21:32:04 +0530

    Time: 281 ms (22.36%), Space: 41.5 MB (32.77%) - LeetHub

---
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int longestBalanced(string s) {
+        
+        int result = 0;
+        int flag;
+
+
+        for (int i =0; i<s.size(); i++){
+        unordered_map <char , int> m;
+
+            for (int j =i; j<s.size(); j++){
+
+                flag = 1;
+
+                m[s[j]]++;
+
+                int cnt = m[s[j]];
+
+                for (auto k : m){
+                    if (k.second != cnt){
+                        flag = 0;
+                        break;}
+                }
+
+                if (flag){
+                    result = max(result , j-i+1);
+                }
+
+
+            }
+
+
+        }
+
+        return result;
+
+
+    }
+};
