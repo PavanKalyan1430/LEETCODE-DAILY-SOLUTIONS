227c7315bf3ea7e0c25ba2aecc69071a237539ac
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2025-10-29 17:48:59 +0530

    Time: 0 ms (100%), Space: 8.4 MB (30.21%) - LeetHub

---
+++ 3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int smallestNumber(int n) {
+        
+        for (int i =0 ;i<= 20 ;i++){
+
+            if ( (pow(2,i) - 1 ) >= n){
+                return (pow(2,i) - 1 );
+            } 
+        }
+
+        return 0;
+
+    }
+};
