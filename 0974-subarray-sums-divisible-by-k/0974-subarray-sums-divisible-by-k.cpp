839996fe68018477e7864857d345b666837bd65f
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2025-10-31 23:01:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        
+
+        int prefix_sum = 0;
+        int result = 0;
+        unordered_map <int , int> m;
+        m[0] = 1;
+
+        for (int i=0; i<nums.size(); i++){
+            
+            prefix_sum += nums[i];
+
+            int comp = prefix_sum % k;
+
+            if ( m.count(comp)){
+                result += m[comp];
+            }
+
+            m[prefix_sum]++;
+
+        }
+
+        return result;
+    }
+};
