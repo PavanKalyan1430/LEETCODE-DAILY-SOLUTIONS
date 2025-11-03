c078c32bcb3069a2827bcd9c7893330e77507f29
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2025-11-03 19:27:31 +0530

    Time: 3 ms (40.27%), Space: 11.3 MB (73%) - LeetHub

--- 0890-find-and-replace-pattern/0890-find-and-replace-pattern.cpp
+++ 0890-find-and-replace-pattern/0890-find-and-replace-pattern.cpp
@@ -2,39 +2,49 @@ class Solution {
 public:
     vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
         
-        vector<string> result;
+
+        vector <string> result;
+
 
         for (int i=0; i<words.size(); i++){
 
+            bool flag = true;
             unordered_map <char, char> m;
-             unordered_map <char, char> n;
-             bool flag = true;
+            unordered_map <char, char> n;
 
+            for (int j =0; j<words[i].size(); j++){
 
-            for (int j=0; j<words[i].size(); j++){
+               if (m.count(words[i][j])){
+                    if (m[words[i][j]] != pattern[j]){
+                        flag = false;
+                        break;
+                    }
+               }
 
-                if (m.count(words[i][j])){
-                    if (m[words[i][j]] != pattern[j] ) flag = false;
-                }
+               else {
+                    m[words[i][j]] = pattern[j];
 
-                else m[words[i][j]] = pattern[j];
+               }
 
-                if (n.count(pattern[j])){
-                    if (n[pattern[j]] != words[i][j] ) flag = false;
-                }
+               if (n.count(pattern[j])){
+                  if (n[pattern[j]] != words[i][j] ) {
+                    flag = false;
+                    break;
+                  }
+               }
 
+               else {
+                    n[pattern[j]] = words[i][j];
 
-                else n[pattern[j]] = words[i][j];
-            }    
+               }
 
-            if (flag) result.push_back(words[i]);    
-            
+            }
+
+            if (flag) result.push_back(words[i]);
+
 
         }
 
         return result;
-
-        
-
     }
 };
