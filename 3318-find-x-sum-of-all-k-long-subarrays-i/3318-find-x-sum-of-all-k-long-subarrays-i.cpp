de337ee0b172b56f346203bf19aa178ff7ca14c5
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2025-11-04 12:03:51 +0530

    Time: 189 ms (5.11%), Space: 34.4 MB (56.55%) - LeetHub

--- 3318-find-x-sum-of-all-k-long-subarrays-i/3318-find-x-sum-of-all-k-long-subarrays-i.cpp
+++ 3318-find-x-sum-of-all-k-long-subarrays-i/3318-find-x-sum-of-all-k-long-subarrays-i.cpp
@@ -1,11 +1,41 @@
 
-struct cmp {
-    bool operator()(pair<int,int>& a, pair<int,int>& b) {
-        if (a.first == b.first)
-            return a.second < b.second; // larger element first if freq same
-        return a.first < b.first;       // higher frequency first
+// struct cmp {
+//     bool operator()(pair<int,int>& a, pair<int,int>& b) {
+//         if (a.first == b.first)
+//             return a.second < b.second; // larger element first if freq same
+//         return a.first < b.first;       // higher frequency first
+//     }
+// };
+
+
+
+int calculate_sum (unordered_map <int,int> &m, int x){
+
+    priority_queue<pair<int, int> , vector<pair<int,int>>> q;
+
+    for(auto i: m){
+        q.push({i.second,i.first});
     }
-};
+
+    int c = x;
+    int sumy = 0;
+
+    while(c > 0 and !q.empty()){
+       cout<<q.top().first <<"   "<< q.top().second<<endl;
+        sumy += (q.top().first * q.top().second);
+        q.pop();
+        c--;
+    }
+
+    cout<<endl;
+
+    return sumy;
+}
+
+
+
+
+
 
 
 class Solution {
@@ -13,33 +43,23 @@ public:
     vector<int> findXSum(vector<int>& nums, int k, int x) {
         
         vector<int> result;
+        int n= nums.size(), ans;
+        unordered_map <int, int> m;
+
+        for (int i=0; i<k; i++) m[nums[i]]++;
+
+        ans = calculate_sum(m, x);
+        result.push_back(ans);
 
 
-        for (int i =0; i<=nums.size()-k; i++){
+
+        for (int i =1; i<=n-k; i++){
+
+            m[nums[i-1]]--;
+            m[nums[i+k-1]]++;
             
-            unordered_map <int, int> m;
-            priority_queue<pair<int, int> , vector<pair<int,int>>, cmp> q;
-
-            for (int j=i; j<i+k; j++){
-                m[nums[j]]++;
-            }
-
-            for(auto i: m){
-                q.push({i.second,i.first});
-               //cout<<i.first <<" " <<i.second<<endl;
-            }
-
-            int c = x;
-            int sumy = 0;
-
-            while(c > 0){
-                cout<<q.top().first <<"   "<< q.top().second<<endl;
-                sumy += (q.top().first * q.top().second);
-                q.pop();
-                c--;
-            }
-
-            result.push_back(sumy);
+            ans = calculate_sum(m, x);
+            result.push_back(ans);
           
 
 
