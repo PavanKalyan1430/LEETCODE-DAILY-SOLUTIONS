f653494a577d41d7069af5938ecccda14716166a
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2025-12-02 22:34:48 +0530

    Time: 0 ms (100%), Space: 7.9 MB (55.25%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,24 +2,20 @@ class Solution {
 public:
     int climbStairs(int n) {
         
-        if(n==0 or n==1) return 1;
+        int x = 1;
+        int y = 2;
+        int z;
 
-        vector<int> dp(n+1, 0);
+        if ( n == 1 || n ==2 ) return n;
 
-        dp[0] = 1;
-        dp[1] = 1;
 
-        for (int i=2; i<=n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+
+        for (int i = 3; i<=n; i++){
+            z  = x+y;
+            x = y;
+            y = z;
         }
 
-        return dp[n];
-
-
-
-
-
-        
-
+        return z;
     }
 };
