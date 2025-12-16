b781a597b61377719a21ab87257ec3ed32ed77e8
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2025-12-16 19:17:51 +0530

    Time: 39 ms (43.05%), Space: 53 MB (12.37%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -5,12 +5,14 @@ public:
         int n = coins.size();
         int m = amount;
 
-        vector<vector<int>> dp( n+1, vector<int> (m+1, INT_MAX-1));
+        vector<vector<int>> dp( n+1, vector<int> (m+1, 10001));
 
         dp[0][0] = 0;
 
+        for (int i=0; i<=n; i++) dp[i][0] = 0;
+
         for (int i=1; i<=n ;i++){
-            for (int j=0; j<=m; j++){
+            for (int j=1; j<=m; j++){
 
                 if ( coins[i-1] > j) dp[i][j] = dp[i-1][j];  //  cant pick cuz EX: we can never from an amount of 1 rs from 2 rupee coin
 
@@ -21,11 +23,11 @@ public:
             }
         }
 
-        for (auto i : dp){
-            for (auto k: i) cout<<k<<" ";
-            cout<<endl;
-        }
+        // for (auto i : dp){
+        //     for (auto k: i) cout<<k<<" ";
+        //     cout<<endl;
+        // }
 
-        return (dp.back().back() == INT_MAX-1) ? -1 : dp.back().back();
+        return (dp.back().back() == 10001) ? -1 : dp.back().back();
     }
 };
