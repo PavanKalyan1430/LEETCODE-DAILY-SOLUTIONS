cbee4c8f387b44d6750d921d0c1f03cf2aa69f63
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2025-12-19 19:38:11 +0530

    Time: 0 ms (100%), Space: 52.2 MB (65.38%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -9,6 +9,8 @@ public:
 
         for (int i=0; i<nums.size()-1; i++){      
             maxi = max(maxi, nums[i] + i);
+
+            if (maxi < i+1) return false;
         }
 
         if (maxi >= n) return true;
