abf1b3e25de77b9d1b0961d583645714ed5747d9
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2025-12-25 16:19:06 +0530

    Time: 3 ms (59.85%), Space: 28.2 MB (41.97%) - LeetHub

---
+++ 3396-minimum-number-of-operations-to-make-elements-in-array-distinct/3396-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+         unordered_map <int, int> m;
+        int n = nums.size();
+        int cnt = 0;
+        int left = 0;
+        
+        for (int i=0; i<nums.size(); i++){
+            m[nums[i]]+=1;
+        }
+
+        for (int right= 0; right<nums.size(); right++){
+
+            while(m[nums[right]] > 1){
+                    cnt +=1;
+
+                for (int j = left; j<left+3 && j<n ; j++){
+                    m[nums[j]]-=1;  
+                }
+                left = left +3;
+                
+            }
+            
+        }
+
+        return cnt;
+    }
+};
