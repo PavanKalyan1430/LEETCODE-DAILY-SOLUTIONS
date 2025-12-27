792b03ed207b5c25dc5bbfb77c0f08a9049f7aeb
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2025-12-27 23:47:29 +0530

    Time: 11 ms (13.52%), Space: 18.2 MB (71.05%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& edges) {
+        int e = edges.size();
+
+        vector<vector<int>> adjlist(n);
+        vector<int> result;
+        vector<int> indegree(n);
+
+        queue<int> q;
+
+        for (int i=0; i<e; i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+
+            adjlist[u].push_back(v);
+            indegree[v]+=1;
+        }
+
+        for (int i=0; i<indegree.size(); i++){
+            if (indegree[i] == 0) q.push(i);
+        }
+
+        while(!q.empty()){
+            int node = q.front();  q.pop();
+            result.push_back(node);
+
+            for (auto neighbour : adjlist[node]){
+                indegree[neighbour]-=1;
+
+                if (indegree[neighbour] == 0) q.push(neighbour);
+            }
+        }
+
+        reverse(result.begin(), result.end());
+        if (result.size() != n) return {};
+
+        return result ;
+    }
+};
+
