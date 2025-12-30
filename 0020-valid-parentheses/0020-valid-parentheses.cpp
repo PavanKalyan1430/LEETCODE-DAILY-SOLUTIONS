db5036ea5e84f65a8f4b42e442a0618bed2ab193
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2025-12-30 14:06:19 +0530

    Time: 0 ms (100%), Space: 8.8 MB (86.82%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,38 +1,27 @@
 class Solution {
 public:
     bool isValid(string s) {
-        
-        string a, b , c , t= "";
-    
+
+        string res = "";
 
         for (int i=0; i<s.size(); i++){
-            int z = t.size();
 
-            if (s[i] == '(') t.push_back('(');
-
-            else if (s[i] == ')'){
-                if (z && t.back() == '(') t.pop_back();
-                else return false;
+            if (s[i] == '(' || s[i] == '{'|| s[i] == '['){
+                res.push_back(s[i]);
             }
 
-            if (s[i] == '{') t.push_back('{');
+            else {
+                if (res.empty()) return false;
 
-            else if (s[i] == '}'){
-                if (z &&t.back() == '{') t.pop_back(); 
-                else return false;
+                if (s[i] == ')' && res.back() != '(')  return false;               
+                else if (s[i] == '}' && res.back() != '{') return false;
+
+                else if (s[i] == ']' && res.back() != '[') return false;   
+
+                res.pop_back();            
             }
-
-            if (s[i] == '[') t.push_back('[') ;
-
-            else if (s[i] == ']'){
-                if (z && t.back() == '[') t.pop_back();
-                else return false;
-            }
-
         }
 
-        return t.size() == 0;
-        
-
+        return res.size() == 0;
     }
 };
