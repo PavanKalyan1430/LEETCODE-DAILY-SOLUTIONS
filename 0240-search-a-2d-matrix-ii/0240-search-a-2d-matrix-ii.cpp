1fc13e0a00ccd3745b3ed3de92acd92e9f31cc5f
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-01-17 15:14:30 +0530

    Time: 52 ms (63.01%), Space: 18.6 MB (66.9%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -3,26 +3,24 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         
         int rows = matrix.size();
-        int col = matrix[0].size();
+        int cols = matrix[0].size();
 
         int r = 0;
-        int c = col-1;
+        int c  = cols-1;
 
-        int end = rows*col-1;
+        while ( r < rows && c >= 0){
 
+            if (matrix[r][c] == target) return true;
 
+            else if (matrix[r][c] > target) c -=1;
 
-        while (r <rows and c>=0){
-
-            if (matrix[r][c] == target ) return true;
-
-            else if ( matrix[r][c] >  target ) c--;
-
-            else r++;
+            else r +=1;
 
         }
 
         return false;
+
+
 
 
 
