aa5ea22391f69676bc5efc4bec49caaff5e22ba9
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-01-18 18:51:55 +0530

    Time: 1 ms (43.63%), Space: 17.9 MB (25.02%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.cpp
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -1,40 +1,36 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
- void TreePaths(TreeNode* root, vector<string>&result, string temp){
+void DFS(TreeNode* root, vector<string>&result, string temp){
 
-        if (!root) return ;
+    if (!root) return;
 
-        if (root->left or root->right)temp = temp + to_string(root->val) + "->";
-        else temp = temp + to_string(root->val);
+    if (!root->left && !root->right){
+        temp += to_string(root->val);
+        result.push_back(temp);
+        return;
+    }
 
-        if (!root->left and !root->right) result.push_back(temp);
+    else temp += (to_string(root->val) + "->");
 
-        TreePaths(root->left, result, temp);
-        TreePaths(root->right, result, temp);
+    DFS(root->left, result, temp );
+
+    DFS(root->right, result, temp);
+    
+
+   
+
+    return;
+    
 
 
- }
+}
 class Solution {
 public:
     vector<string> binaryTreePaths(TreeNode* root) {
-        
-        vector<string> result ;
+        vector<string> result;
+
         string temp = "";
 
-        TreePaths(root, result, temp);
+        DFS(root, result, temp);
 
         return result;
-
-
-
     }
 };
