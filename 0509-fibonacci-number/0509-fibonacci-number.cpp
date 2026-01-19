a17d83a6906136b8486f08e564f9d3257fd2ac7d
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-01-19 10:50:24 +0530

    Time: 11 ms (36.41%), Space: 7.7 MB (87.24%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,27 +1,11 @@
-int fun(int n , vector<int>& dp){
-
-    if (n == 0 || n== 1) return n;
-
-    if (dp[n] != -1) return dp[n];
-
-    int first = fun(n-1, dp) ;
-    int second = fun(n-2, dp);
-
-    dp[n] = first + second;
-
-    return dp[n];
-
-
-}
-
 class Solution {
 public:
     int fib(int n) {
         
-        if (n == 0 || n== 1) return n;
-
-        vector<int> dp(n, -1);
+        if (n == 0  or n==1) return n;
 
         return fib(n-1) + fib(n-2);
+
+
     }
 };
