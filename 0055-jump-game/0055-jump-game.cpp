1311bfcc696aebd9d39ff0cec6707a7df69c51cb
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-01-19 12:10:58 +0530

    Time: 517 ms (10.23%), Space: 55.8 MB (6.09%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,22 +1,33 @@
+bool can_reach(vector<int>&nums, vector<int>&dp, int idx, int max_reach){
+
+    if (idx >= nums.size()-1) return true;
+
+    if (nums[idx] == 0){
+        return false;}
+
+    if (dp[idx] != true) return false;
+
+    max_reach = nums[idx] + idx;
+
+    for (int jump = idx+1; jump<=max_reach; jump++){
+        if (can_reach (nums, dp,jump, max_reach) == true) return true;
+    } 
+    
+    return dp[idx] = false;
+    
+   
+
+}
+
+
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
         
-        int n = nums.size()-1;
-        int reached = n;
 
-        int maxi = 0;
+        vector<int> dp (nums.size(), 1);
 
-        for (int i=0; i<nums.size()-1; i++){      
-            maxi = max(maxi, nums[i] + i);
-
-            if (maxi < i+1) return false;
-        }
-
-        if (maxi >= n) return true;
-        else return false;
-
-
+        return can_reach(nums, dp, 0, 0);
 
     }
 };
