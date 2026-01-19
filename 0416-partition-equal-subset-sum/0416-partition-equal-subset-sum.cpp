c57c254810167b2862eaed753213e17d379e8be3
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-01-19 18:51:27 +0530

    Time: 143 ms (39.77%), Space: 110.3 MB (43.7%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,35 +1,38 @@
+bool fun(vector<int>&nums,  int sum , int i, vector<vector<int>>&dp){
+
+    if (sum == 0) return true;
+
+    if (i < 0) return false;
+
+    if (dp[i][sum] != -1) return dp[i][sum];
+
+    if (nums[i] <= sum){
+        int pick = fun(nums,  sum - nums[i], i-1, dp);
+        int notpick = fun(nums ,  sum  , i-1, dp);
+
+        return dp[i][sum] = pick || notpick;
+    }
+
+    else return dp[i][sum] =  fun(nums,  sum , i-1, dp);
+}
+
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int n = nums.size();
-        long long total = 0;
-        for (int num : nums) total += num;
+        
 
-        // If total sum is odd, can't partition equally
+        int total = 0;
+        for (int i : nums) total +=i;
+
         if (total % 2 != 0) return false;
 
-        int target = total / 2;
+        int sum = total/2;
 
-        // Use long long DP array to count subsets (treat count > 0 as "true")
-        vector<vector<long long>> dp(n + 1, vector<long long>(target + 1, 0));
+        vector<vector<int>> dp(nums.size(), vector<int>(sum+1, -1));
 
-        // Base case: there's always 1 way to form sum 0 (empty subset)
-        for (int i = 0; i <= n; i++) dp[i][0] = 1;
 
-        // Fill DP table using 0/1 Knapsack pattern
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= target; j++) {
-                if (nums[i - 1] <= j) {
-                    // pick or not pick
-                    dp[i][j] = dp[i - 1][j] + dp[i - 1][j - nums[i - 1]];
-                } else {
-                    // not pick only
-                    dp[i][j] = dp[i - 1][j];
-                }
-            }
-        }
+        return fun(nums, sum , nums.size()-1, dp);
 
-        // If count > 0, subset exists
-        return dp[n][target] > 0;
+        
     }
 };
