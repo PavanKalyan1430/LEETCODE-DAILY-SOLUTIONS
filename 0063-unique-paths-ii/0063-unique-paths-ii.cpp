48a5d799f1db44c32d202a2cd75004ade2d72613
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-01-19 12:23:14 +0530

    Time: 0 ms (100%), Space: 11.9 MB (29.06%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& Grid) {
-        
-        int m = Grid.size();        // rows size
-        int n = Grid[0].size();     // columns size
-
-        vector<vector<int>> DP(m,vector<int> (n,0));
-
-        if (Grid[0][0] == 1) return 0;
-
-        for (int i=0; i<m; i++){
-           if (Grid[i][0] != 1) DP[i][0] = 1; 
-           else break; 
-        }
+    int uniquePathsWithObstacles(vector<vector<int>>& nums) {
         
 
-        for (int j =0; j<n; j++){
-            if (Grid[0][j] != 1) DP[0][j] = 1;
+        if (nums[0][0] == 1 || nums.back().back() == 1) return 0;
+
+        int rows = nums.size();
+        int cols = nums[0].size();
+
+        vector<vector<int>> dp(rows, vector<int>(cols, 0));
+
+
+        for (int i=0; i<rows; i++){
+            if (nums[i][0] != 1) dp[i][0] = 1;
             else break;
         }
 
+        for (int i=0; i<cols; i++){
+            if (nums[0][i] != 1) dp[0][i] = 1;
+            else break;
+        }
 
-        for (int i=1; i<m; i++){
-            for (int j=1; j<n; j++){
+        for (int i=1; i<rows; i++){
+            for (int j = 1; j<cols; j++){
+                
+                if (nums[i][j] == 1) continue;
 
-                if (Grid[i][j] != 1){
-                    DP[i][j] = DP[i-1][j] + DP[i][j-1];
-                }
-
-                else DP[i][j] = 0;
+                dp[i][j] = dp[i][j-1] + dp[i-1][j];
             }
         }
 
-        return DP.back().back();
+        return dp[rows-1][cols-1];
+
+
 
     }
 };
