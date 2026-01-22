bc5c4a3f03fed576ac2ce86deaa7f67cc09a8b2d
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-01-22 17:49:19 +0530

    Time: 44 ms (16.73%), Space: 27.8 MB (6.18%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,36 +1,35 @@
-int fun(string &m, string &n ,int i, int j, vector<vector<int>>&dp){
+int fun (string &s1 , string&s2 , int i , int j, vector<vector<int>>&dp){
 
-    if (i < 0 || j< 0){
-        return 0;
-    }
+    if (i <0 || j<0) return 0;
 
     if (dp[i][j] != -1) return dp[i][j];
 
-    if (m[i] == n[j]){
-        return dp[i][j]  = 1 + fun(m,n, i-1, j-1, dp);
+    if (s1[i] == s2[j]){
+        return dp[i][j] = 1 + fun(s1, s2, i-1, j-1, dp);
     }
 
     else {
+        int pick_1st_string = fun(s1, s2, i, j-1, dp);
+        int pick_2nd_string = fun(s1, s2, i-1, j, dp);
 
-        int pick1st = fun(m,n,i,j-1, dp);
-        int pick2nd = fun(m,n,i-1,j, dp);
-
-        return dp[i][j]  =  max(pick1st, pick2nd);
+        return dp[i][j] = max(pick_1st_string , pick_2nd_string);
     }
+
 
 }
 
 class Solution {
 public:
-    int longestCommonSubsequence(string m, string n) {
+    int longestCommonSubsequence(string str1, string str2) {
         
 
-        int i = m.size();
-        int j  = n.size();
+        int n = str1.size();
+        int m = str2.size();
 
-        vector<vector<int>> dp(i, vector<int>(j+1, -1));
+        vector<vector<int>> dp(n, vector<int>(m+1, -1));
 
-        return fun(m,n, i-1, j-1, dp);
+        return fun(str1 , str2 , n-1, m-1, dp);
+
 
     }
 };
