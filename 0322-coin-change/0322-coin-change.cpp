703b2c84919eb947c002c54959000be41d225fe1
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-01-22 17:33:41 +0530

    Time: 44 ms (36.39%), Space: 53 MB (14.81%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
+    int coinChange(vector<int>& nums, int amount) {
         
-        int n = coins.size();
-        int m = amount;
+        int n = nums.size();
 
-        vector<vector<int>> dp( n+1, vector<int> (m+1, 10001));
+        vector<vector<int>> dp(n+1, vector<int>(amount+1, 0));
 
-        dp[0][0] = 0;
+        for (int i=1; i<=amount ; i++) dp[0][i] = INT_MAX -1;
 
-        for (int i=0; i<=n; i++) dp[i][0] = 0;
+        for (int i=1; i<=n; i++){
+            for (int j = 1; j<= amount ; j++){
 
-        for (int i=1; i<=n ;i++){
-            for (int j=1; j<=m; j++){
-
-                if ( coins[i-1] > j) dp[i][j] = dp[i-1][j];  //  cant pick cuz EX: we can never from an amount of 1 rs from 2 rupee coin
-
-                else {
-                    dp[i][j] = min(dp[i-1][j],  1+ dp[i][j- coins[i-1]]);
+                if (j >= nums[i-1]){
+                    dp[i][j] = min(dp[i-1][j] , 1+ dp[i][j-nums[i-1]]);
                 }
 
+                else dp[i][j] = dp[i-1][j];
             }
         }
 
-        // for (auto i : dp){
-        //     for (auto k: i) cout<<k<<" ";
-        //     cout<<endl;
-        // }
-
-        return (dp.back().back() == 10001) ? -1 : dp.back().back();
+        return (dp.back().back() == INT_MAX-1) ? -1 : dp.back().back();
     }
 };
