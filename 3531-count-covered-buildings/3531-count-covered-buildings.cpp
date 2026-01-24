1dba1615173b1a307f836cef7ba1f7cb24303dce
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-01-24 14:17:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3531-count-covered-buildings/3531-count-covered-buildings.cpp
+++ 3531-count-covered-buildings/3531-count-covered-buildings.cpp
@@ -2,59 +2,51 @@ class Solution {
 public:
     int countCoveredBuildings(int n, vector<vector<int>>& buildings) {
 
-            if (buildings.size() < 5) return 0;
-            int cnt = 0;
+        int b = buildings.size();
+        int cnt = 0;
 
-            unordered_map <int , vector<pair<int , int>>> mx;
-            unordered_map <int , vector<pair<int , int>>> my;
+        if (b < 5 || n < 3) return 0;
 
-           map <pair<int,int> , int> result;
+        vector<int> maxcolY (n+1, INT_MIN);
+        vector<int> maxrowX (n+1, INT_MIN);
 
-            //unordered_map <int , int> res;
+        vector<int> mincolY (n+1, INT_MAX);
+        vector<int> minrowX (n+1, INT_MAX);
 
+        for (auto i : buildings){
+            int x  = i[0];
+            int y  = i[1];
 
+            maxrowX[y]= max(maxrowX[y], x);
+            minrowX[y] = min(minrowX[y], x);
 
-            for (auto i : buildings){
-                int x = i[0];
-                mx[x].push_back( {i[0] , i[1]});
-            }
+            maxcolY[x] = max(maxcolY[x], y);
+            mincolY[x] = min(mincolY[x], y);
+        }
 
-            for (auto i : buildings){
-                int y = i[1];
-                my[y].push_back( {i[0], i[1]} );
-            }
+        for (auto i : buildings){
+            int x = i[0];
+            int y = i[1];
 
-            for (auto i : mx){
-                vector<pair<int , int>> v= i.second;
-                sort(v.begin(), v.end());
+            bool have_col= false;
+            bool have_row = false;
 
-                for (int i=1; i<v.size()-1; i++) result[v[i]] = 1;
-            }
+            if (x > minrowX[x] && x< maxrowX[x]) have_row = true;
 
-            for (auto i : my){
-                vector<pair<int , int>> v= i.second;
-                sort(v.begin(), v.end());
-                for (int i=1; i<v.size()-1; i++) if (result.count(v[i])) cnt +=1;
-            }
+            if (y > mincolY[y] && y < maxcolY[y]) have_col = true;
 
-            return cnt;
+            if (have_row && have_col) cnt +=1;
 
+        }
 
+        // for (int i: minrowX) cout<<i<<" ";
+        // cout<<endl;
+        // for (int i: maxrowX) cout<<i<<" ";
 
-
-            
-
-
+        return cnt;
 
 
 
 
     }
 };
-
-
-// The hint is a bit misleading. There's no need to sort the group. There's an O(N) solution to this and to me it was more intuitive than the hints. Look at the diagram. For a point to be covered, what properties must its row and column satisfy?
-
-// I dont want to give away too much but the idea here is that it has to be bounded in all 4 directions. So for each row and column we need the maximum and minimum value (think about how we can store and compute this efficiently). Then think about what we can do once we have the min and max value to check if the given constraint is satisfied.
-
-// If you are still stuck, try drawing out a diagram by hand.
