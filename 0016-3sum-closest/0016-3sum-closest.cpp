f6d5b91c50250b623fb73cbacb4f95d3ebc4f743
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-01-28 14:48:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -2,39 +2,45 @@ class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
         
+        int result = -1;
         int mindiff = INT_MAX;
-        int closest;
+        int n = nums.size();
 
         sort(nums.begin(), nums.end());
-        
-        for (int i=0; i<nums.size()-2; i++){
+
+
+        for (int i=0; i<n-2; i++){
+
+            if (i != 0 && nums[i] == nums[i-1]) continue;
 
             int left = i;
             int mid = i+1;
-            int right = nums.size()-1;
+            int right = n-1;
 
-            while(mid < right){
-                
-                int ele = nums[left] + nums[mid] + nums[right];
+            while (mid < right){
+                int sum = nums[left] + nums[mid] + nums[right];
 
-                if (ele == target) return ele;
+                int diff = abs (target - sum);
 
-                int diff = abs(ele - target);
+                if (sum == target) return 0 ;
 
                 if (diff < mindiff){
+                    result = sum;
                     mindiff = diff;
-                    closest = ele;
                 }
 
-                if (ele < target) mid++;
-                else right--;
+                else if (sum > target){
+                    //result = min(result , sum-target);
+                    right-=1;
+                }
 
-
+                else{
+                    //result  = min(result , target- sum);
+                    mid +=1;
+                }
             }
-
         }
 
-            return closest;
-
+        return result;
     }
 };
