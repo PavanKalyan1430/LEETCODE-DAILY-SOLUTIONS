7788032b8248da9eb0c288bdc867bf36f6d5552a
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-01-30 19:28:38 +0530

    Time: 3 ms (9.26%), Space: 31.7 MB (87.38%) - LeetHub

--- 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.cpp
+++ 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.cpp
@@ -1,46 +1,38 @@
-void FUN(int i, int j , int k, vector<vector<int>>&grid, bool &ans){
-
-    int rows = grid.size();
-    int cols = grid[0].size();
-
-    if (ans) return;
-
-    if (i>=rows || j >= cols || i<0 || j<0 || grid[i][j] != k) return ;
-
-   
-    if (k == (rows*cols - 1)){
-        ans  = true;
-        return;
-    }
-
-                 FUN(i+2, j+1, k+1, grid, ans) ; 
-                 FUN(i+2, j-1, k+1 , grid, ans) ;
-                 FUN(i-2, j+1, k+1 , grid, ans) ;
-                 FUN(i-2, j-1, k+1 , grid, ans) ;
-
-                 FUN(i+1, j+2, k+1 , grid, ans) ;
-                 FUN(i+1, j-2, k+1 , grid, ans) ;
-                 FUN(i-1, j+2, k+1 ,grid, ans)  ;
-                 FUN(i-1, j-2, k+1 , grid, ans) ;
-
-    return ;
-
-}
-
-
 class Solution {
 public:
     bool checkValidGrid(vector<vector<int>>& grid) {
+        int n = grid.size();
 
-        if (grid[0][0] != 0) return false;
+    if (grid[0][0] != 0) return false;
 
-        //unordered_map <int , pair<int,int>> m;
+    vector<pair<int,int>> moves = {
+        {2,1}, {2,-1}, {-2,1}, {-2,-1},
+        {1,2}, {1,-2}, {-1,2}, {-1,-2}
+    };
 
-        int rows = grid.size();
-        int cols = grid[0].size();
-        bool ans = false;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
 
-        FUN(0,0,0,grid, ans);
-        return ans;
+            int k = grid[i][j];
+            if (k == n*n - 1) continue;
+
+            bool found = false;
+
+            for (auto [dx, dy] : moves) {
+                int x = i + dx;
+                int y = j + dy;
+
+                if (x >= 0 && y >= 0 && x < n && y < n &&
+                    grid[x][y] == k + 1) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) return false;  // fail fast
+        }
+    }
+
+    return true;
+
     }
 };
