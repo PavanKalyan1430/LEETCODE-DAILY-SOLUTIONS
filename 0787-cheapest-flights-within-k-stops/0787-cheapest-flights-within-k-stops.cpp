784c5a6cd829e1bc3ca8db5377daf5884edde3e6
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-02-05 19:13:04 +0530

    Time: 8 ms (22.45%), Space: 17.8 MB (75.12%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,32 +1,56 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-    vector<vector<pair<int, int>>> adj(n);
+    int findCheapestPrice(int n, vector<vector<int>>& edges, int src, int dst, int k) {
+        
+        vector<vector<pair<int,int>>> adjlist(n);
 
-    for (auto& flight : flights) {
-        int u = flight[0], v = flight[1], w = flight[2];
-        adj[u].push_back({v, w});
-    }
+        vector<int> distance(n, INT_MAX);
 
-    priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> pq;
-    pq.push({0, src, 0});
+        priority_queue<vector<int>, vector<vector<int>>, greater<> > q;
 
-    vector<int> stops(n, INT_MAX);
+        for (int i=0; i<edges.size(); i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            int dist = edges[i][2];
 
-    while (!pq.empty()) {
-        auto [cost, node, stop] = pq.top(); pq.pop();
-
-        if (node == dst) return cost;
-        if (stop > k || stop > stops[node]) continue;
-
-        stops[node] = stop;
-
-        for (auto& [nei, price] : adj[node]) {
-            pq.push({cost + price, nei, stop + 1});
+            adjlist[u].push_back({v, dist});
         }
+        //{stops , dist,  src}
+        q.push({0,0,src});
+
+        while (!q.empty()){
+            auto v = q.top();   q.pop();
+            int stops = v[0];
+            int d     = v[1];
+            int node  = v[2];
+
+            //auto [stops , d, node] = q.top(); q.pop();
+            //if (node == dst) return distance[node];
+            //int newstop;
+
+            for (auto neighbour : adjlist[node]){
+                auto [adjnode , adjdist] = neighbour;
+
+                if (stops == k+1) continue;
+                //cout<<stops << "   "<< adjnode<<endl;
+
+                if (d + adjdist < distance[adjnode]){
+                    distance[adjnode] = d + adjdist;
+                    q.push({stops +1 , distance[adjnode], adjnode});
+                }
+            }
+
+        }
+
+        if (distance[dst] == INT_MAX) return -1;
+        return distance[dst];
+
+        //return -1;
+
+
+
+
+
+
     }
-
-    return -1;
-}
-
 };
