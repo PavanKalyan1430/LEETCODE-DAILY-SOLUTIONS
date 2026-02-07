dff619ea57463c6090c38bcf57416d18dbf1a690
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-02-07 22:03:36 +0530

    Time: 963 ms (34.67%), Space: 120 MB (25.75%) - LeetHub

--- 1928-minimum-cost-to-reach-destination-in-time/1928-minimum-cost-to-reach-destination-in-time.cpp
+++ 1928-minimum-cost-to-reach-destination-in-time/1928-minimum-cost-to-reach-destination-in-time.cpp
@@ -1,55 +1,59 @@
 class Solution {
 public:
-    int minCost(int maxTime, vector<vector<int>>& edges, vector<int>& Fees) {
-        
-        // int rows = grid.size();
-        // int cols = grid[0].size();
+    int minCost(int maxTime, vector<vector<int>>& edges, vector<int>& fees) {
 
-        int n = Fees.size();
+        
+        int n = fees.size();
         vector<vector<pair<int,int>>> adjlist(n);
-        unordered_map <int ,int> t;
+
+        vector<vector<int>> dp(n, vector<int>(maxTime + 1, INT_MAX));
+        dp[0][0] = fees[0];
 
         for (int i=0; i<edges.size(); i++){
             int u = edges[i][0];
             int v = edges[i][1];
             int d = edges[i][2];
 
-            adjlist[u].push_back({v,d});
+            adjlist[u].push_back({v,d}); 
             adjlist[v].push_back({u,d});
+ 
         }
 
-    
-        
 
-       priority_queue<tuple<int,int,int>, vector<tuple<int,int,int>>, greater<>> q;
-        // q.push(cost, time , node);
-        q.push({Fees[0],0,0});
+        priority_queue<tuple<int,int,int>, vector<tuple<int,int,int>> , greater<> > q;
+
+        q.push({fees[0], 0, 0});
 
         while (!q.empty()){
-            auto [c , time , node] = q.top();
-            q.pop();
+            auto [c,time,node] = q.top();  q.pop();
+
+           // int c = v[0];   int time = v[1];    int node = v[2];
 
             if (node == n-1) return c;
+            if (c > dp[node][time]) continue;
+
+
 
 
             for (auto neighbour : adjlist[node]){
-                auto [adjnode, adjtime] = neighbour;
 
-                int new_time = time + adjtime;
+                auto [adj_node, adj_time] = neighbour;
 
-                if (new_time  > maxTime) continue;
+                int new_time = time + adj_time ;
+                int new_cost = c + fees[adj_node];
+                if (new_time > maxTime) continue;
 
-                int new_cost = c + Fees[adjnode];
+                if (dp[adj_node][new_time] <= new_cost) continue;
 
-                if (!t.count(adjnode) ||  t[adjnode] > new_time ){
-                    t[adjnode] = new_time;
-                    q.push({new_cost , new_time , adjnode});
-                }
-            }
-                
+                dp[adj_node][new_time] = new_cost;
+                q.push({new_cost , new_time , adj_node});
 
+
+
+            } 
         }
-      
-         return -1;
+
+        return -1;
+
     }
 };
