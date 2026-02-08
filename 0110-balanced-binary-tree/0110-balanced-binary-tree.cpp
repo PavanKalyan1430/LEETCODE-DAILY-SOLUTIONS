0352cfecb8f978bb33022c2009031e6ae9ceb4ac
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-02-08 18:45:07 +0530

    Time: 0 ms (100%), Space: 23 MB (54.19%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -1,24 +1,37 @@
-int depth(TreeNode* root, bool &check){
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 
-    if ( root == NULL) return 0;
+int fun(TreeNode* root, bool &flag){
 
-    int left = depth(root->left , check);
-    int right = depth(root->right, check);
+    if (flag == false) return 0;
+    if (!root) return 0;
 
-    if ( abs(left - right) > 1 ) check = false;
+    int left =  fun(root->left, flag);
+    int right = fun(root->right, flag);
 
-    return max(left, right) + 1;
+    if (abs (left - right) > 1) flag = false;
+
+    return 1 + max(left , right);
+
+
 }
 
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
-
-        bool check = true;
-        int h = depth(root, check);
-
-        return check;
-
+        
+        bool flag = true;
+        fun(root, flag);
+        return flag;
 
     }
 };
