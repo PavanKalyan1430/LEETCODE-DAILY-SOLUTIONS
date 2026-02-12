f23060f1eee8eeaf0e1e421528c8cd9044801208
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-02-12 11:38:16 +0530

    Time: 2 ms (71.13%), Space: 12.1 MB (55.61%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,32 +1,20 @@
-void fun(vector<int>& nums,  vector<int>& temp,  vector<vector<int>>& result, vector<bool>& check, int n){
+void fun(vector<int>&nums, set<vector<int>>&result, int idx){
 
-    if ( temp.size() == n){
+    if (idx == nums.size()){
+        if (!result.count(nums)) result.insert(nums);
+        return;
+    }
 
-        result.push_back(temp);
-        
+
+    for (int i = idx; i<nums.size(); i++){
+
+        if (i != idx && nums[i] == nums[i-1]) continue;
+
+        swap(nums[i], nums[idx]);
+        fun(nums, result, idx+1);
+        swap(nums[i], nums[idx]);
+    }
     
-        return;}
-    
-
-    for (int i=0; i<n; i++){
-
-        if (check[i]) continue;
-        
-        if ( i > 0 and nums[i] == nums[i-1] and check[i-1] == true) continue;
-
-            temp.push_back(nums[i]);
-            check[i] = true;
-
-            fun(nums, temp, result, check, n);
-
-            temp.pop_back();
-            check[i] = false;
-
-        }
-
-    
-
-    return;
 
 }
 
@@ -34,18 +22,12 @@ void fun(vector<int>& nums,  vector<int>& temp,  vector<vector<int>>& result, ve
 class Solution {
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        
+       // vector<vector<int>> result;
+        set<vector<int>> result;
         sort(nums.begin(), nums.end());
+        fun(nums, result, 0);
 
-        vector<int> temp;
-        vector<vector<int>> result;
-        vector<bool> check(nums.size());
-        int n = nums.size();
-
-
-        fun(nums, temp, result, check, n);
-
-        return result;
-
+        vector<vector<int>> v(result.begin(), result.end());
+        return v;
     }
 };
