1f4a3cbacb8fd4b543a427dde15e335a1507b77b
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-02-13 14:26:46 +0530

    Time: 3 ms (77.58%), Space: 12.4 MB (95.13%) - LeetHub

--- 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.cpp
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.cpp
@@ -1,40 +1,50 @@
-void fun(vector<int>& nums, vector<int> &result, int digcnt, int number, 
-    vector<bool>& used) {
-
-    if (digcnt == 3){
-        if ( number % 2 == 0) result.push_back(number);
-        return;
-    }
-
-    for (int i= 0 ; i<nums.size(); i++){
-        
-        if (used[i]) continue;
-        if (digcnt == 0 && nums[i] == 0) continue;
-        if (i != 0 && nums[i] == nums[i-1] && !used[i-1]) continue;
-
-        used[i] = true;
-        fun(nums, result, digcnt+1, number*10 + nums[i] , used);
-        used[i]= false;
-
-    }
-
-}
-
-
 class Solution {
 public:
-    vector<int> findEvenNumbers(vector<int>& nums) {
+    vector<int> findEvenNumbers(vector<int>& digits) {
         
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        int start = 0;
-
-        vector<bool> used(n,false);
-
         vector<int> result;
-        int digcnt = 0;
 
-        fun(nums, result, digcnt, 0, used);
+        unordered_map <int, int> m;
+
+        for (auto i : digits) m[i] += 1;
+
+        //cout<<m[4]<<endl;
+
+        int i = 100;
+
+        while (i <= 1000){
+
+            
+            bool flag = true;
+            int rem = i/100;
+            int rem2 = (i/10) % 10;
+            int rem3 = i % 10;
+
+            int temp1, temp2, temp3;
+            temp1 = m[rem];
+            temp2 = m[rem2];
+            temp3 = m[rem3];
+
+            //cout<<rem<<" ";
+            if ( m[rem] <= 0  ){
+                i+= 100;
+                continue;
+            }
+             m[rem]-=1;
+
+            (m[rem2] > 0)  ?  m[rem2]-=1 : flag = false;
+            (m[rem3] > 0)  ?  m[rem3]-=1 : flag = false;
+
+            if (flag) result.push_back(i);
+
+             m[rem] = temp1;
+             m[rem2] = temp2;
+             m[rem3] = temp3;
+            //cout<<i<<" ";
+             i+=2;
+
+
+        }
 
         return result;
     }
