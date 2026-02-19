eeb7b7263e1a691b5200f468b47df0eefafd2048
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-02-19 11:43:16 +0530

    Time: 763 ms (4.11%), Space: 11.8 MB (6.49%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,51 +1,51 @@
-int split(int split_sum , vector<int>&nums){
 
-    int cnt  = 0;
-    int sum = 0;
-
-
-    for (int i=0; i<nums.size(); i++){
-
-        if (sum  + nums[i] > split_sum){
-            cnt +=1;
-            sum = nums[i];
-        }
-
-        else sum += nums[i];
-    }
-
-
-    if (sum > 0) cnt +=1;
-    //cout<<split_sum << "   "<<cnt<<endl;
-
-    return cnt;
-
-
-}
 
 
 
 class Solution {
 public:
+
+    
+
+    int fun (vector<int>& nums ,  int k , int idx, vector<vector<int>> &dp){
+
+    if (idx == nums.size()) return 0;    
+    
+    if (k == 1){
+        int s = 0;
+        for (int  p = idx ; p<nums.size(); p++){
+            s += nums[p];
+        }
+        return s;
+    }
+
+
+    if (dp[idx][k] != -1) return dp[idx][k];
+        
+
+    int ans = INT_MAX;
+    int curr= 0;
+
+    for (int i = idx ; i<nums.size(); i++){
+        curr += nums[i];
+        ans = min(ans,  max(curr , fun(nums, k-1, i+1, dp)));
+    }
+
+    return  dp[idx][k] = ans;
+
+}
+
+
     int splitArray(vector<int>& nums, int k) {
         
-        int left = *max_element(nums.begin(), nums.end());
-        int right = accumulate(nums.begin(), nums.end(), 0);
-
-        int result = 0;
-
-        while(left <= right){
-            int mid = (left + right) / 2;
-
-            if (split(mid, nums) <= k){
-                result = mid;
-                right = mid -1;
-            }
-
-            else left = mid + 1;
-        }
-
-        return result;
         
+        int curr = 0;
+        vector<vector<int>> dp(nums.size(), vector<int>(k+1, -1));
+        int  rem = accumulate(nums.begin(), nums.end(), 0);
+        int idx = 0;
+
+
+        return fun(nums, k, idx, dp);
+
     }
 };
