fe32c16b31a218554d8be365a7c42360f5402b48
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-02-25 12:17:31 +0530

    Time: 6 ms (13.59%), Space: 10.4 MB (90.27%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -4,39 +4,34 @@ public:
         
         int open = 0;
         int close = 0;
-        int result = 0;
-
+        int ans = 0 ;
 
         for (int i=0; i<s.size(); i++){
-            if (s[i] == '(') open +=1;
-            else close +=1;
+            (s[i] == '(') ? open+=1 : close +=1;
 
             if (close > open){
-                open = 0;
-                close = 0;
+                open = 0; close = 0;
             }
 
-            if (open == close) result = max(result , open*2);
+            if (open == close) ans = max(open*2, ans);
         }
 
-        open = 0;
-        close = 0;
-        
-        for (int i= s.size()-1; i>=0; i--){
-            if (s[i] == '(') open +=1;
-            else close +=1;
+        open = 0; close = 0;
 
-            if (open > close){
-                open = 0;
-                close = 0;
+        for (int i=s.size()-1; i>=0; i--){
+
+            (s[i] == '(') ? open+=1 : close +=1;
+
+            if (close < open){
+                open = 0; close = 0;
             }
 
-            if (open == close) result = max(result , close*2);
+            if (open == close) ans = max(open*2, ans);
         }
 
-        return result;
+        return ans;
 
-        // for (int i=0; i<s.size(); i++)
+
 
     }
 };
