26c33665cfa68eb98c0658ec51cdef8583b55e8b
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-02-26 23:30:47 +0530

    Time: 0 ms (100%), Space: 21.4 MB (94.52%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -1,25 +1,27 @@
+bool DFS(TreeNode* root, int &targetSum, int currentSum){
+
+    if ( !root ) return false;
+
+    currentSum+= root->val;
+
+     if (!root->left and !root->right){
+        if (currentSum == targetSum){
+            return true;
+        }
+    }
 
 
+    bool left =  DFS(root->left , targetSum, currentSum);
+    bool right =  DFS(root->right , targetSum, currentSum);
+
+    return left or right;
+}
 
 class Solution {
 public:
-    bool hasPathSum(TreeNode* root, int target) {
-        
-        if (!root) return false;
+    bool hasPathSum(TreeNode* root, int targetSum) {
 
-        target -= root->val;
-
-        if (!root->left and !root->right){
-            if (target == 0) return true;
-
-            
-            
-        }
-        
-        cout<<target<<" ";
-         return hasPathSum(root->left, target) or hasPathSum(root->right, target);
-        
-        
-
+        int currentSum = 0;
+        return DFS(root, targetSum, currentSum);
     }
 };
