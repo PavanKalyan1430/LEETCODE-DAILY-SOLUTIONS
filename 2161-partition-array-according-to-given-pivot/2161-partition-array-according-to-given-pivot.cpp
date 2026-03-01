aaa5ceccad8bc0a1fc317b42c7bcb22d11b28f90
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-03-01 22:25:19 +0530

    Time: 8 ms (51.67%), Space: 127.8 MB (78.6%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -1,39 +1,27 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector <int> res(nums.size());
-        int left=0,right =0;
-        int mid =0;
+        
+        int n = nums.size();
+        vector<int> result(n);
+
+        int less =0, equal= 0; 
 
         for (int i=0; i<nums.size(); i++){
-            if (nums[i] < pivot){
-                left+=1;
-            }
-
-            else if (nums[i]==pivot){
-                mid+=1;
-            }
-        }
-            right = left + mid;
-            mid = left ;
-            left =0;
-        for (int i=0 ; i<nums.size(); i++){
-            if (nums[i] > pivot){
-                res[right] = nums[i];
-                right+=1;
-            }
-            else if (nums[i] < pivot){
-                res[left] = nums[i];
-                left+=1;
-            }
-            else {
-                res[mid] = nums[i];
-                mid+=1;
-            }
+            if (nums[i] < pivot) less+=1;
+            else if (nums[i] == pivot) equal+=1;
         }
 
-    
+        int low = 0; 
+        int mid = less; 
+        int high = less+ equal;
 
-    return res;
+        for (int i=0; i<nums.size(); i++){
+            if (nums[i] < pivot) result[low++] = nums[i];
+            else if (nums[i] == pivot) result[mid++] = nums[i];
+            else result[high++] = nums[i];
+        }
+        return result;
+
     }
 };
