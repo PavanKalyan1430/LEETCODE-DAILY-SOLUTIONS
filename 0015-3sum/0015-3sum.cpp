e36aece9cdf16327bdb60698fb220d1e8c70a25c
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-03-01 19:54:50 +0530

    Time: 44 ms (76.22%), Space: 29.1 MB (45.42%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,43 +1,48 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-
-        int n = nums.size();
         
         sort(nums.begin(), nums.end());
         vector<vector<int>> result;
+        int n = nums.size();
 
-        for (int i=0; i<nums.size()-1; i++){
+        for (int i=0; i<nums.size()-2; i++){
+            
+            if (i!= 0 && nums[i] == nums[i-1]) continue;
 
-            if (i!=0 && nums[i] == nums[i-1]) continue;
 
             int left = i;
             int mid = i+1;
             int right = n-1;
 
-            while(mid < right){
+            while (mid < right){
+                int sum = nums[left] + nums[mid] + nums[right];
 
-                int ele = nums[left] + nums[mid] + nums[right];
-
-                if ( ele == 0){
+                if (sum == 0){
                     result.push_back({nums[left], nums[mid], nums[right]});
-                    mid +=1;
-                    right-=1;    
-                       
-                    while (mid < n && nums[mid] == nums[mid-1]) mid +=1; 
-                    while (right > 0 && nums[right] == nums[right+1]) right-=1;
+
+
+                    while (mid < right && nums[mid] == nums[mid+1]) mid+=1;
+                    while (mid > right && nums[right] == nums[right-1]) right-=1;
+
+                    mid+=1;
+                    right-=1;
+
+
+            
                 }
 
-                else if (ele < 0)  mid+=1;
+                else if (sum < 0) mid+=1;
 
-                else right-=1;
-
-
-
+                else right -=1;
             }
+
+            
         }
 
         return result;
+
+
 
     }
 };
