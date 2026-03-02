0b9ed8d09cc43466592db99d32c8ef43671d1152
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-03-02 12:28:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,35 +1,50 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        
-        int required = t.size();
-        int start , min_len = INT_MAX, left = 0;
-        vector<int> freq(130, 0);
 
+        if (s.size() < t.size()) return "";
 
-        for (int i =0; i<t.size(); i++) freq[t[i]]+=1;
+        vector<int> freq(128, 0);
 
-        for (int right = 0; right<s.size(); right++){
-            
+        // Build requirement frequency
+        for (char c : t)
+            freq[c]++;
+
+        int left = 0;
+        int start = 0;
+        int min_len = INT_MAX;
+
+        int required = t.size();  // total characters still needed
+
+        for (int right = 0; right < s.size(); right++) {
+
+            // If this character was needed, reduce required
+            if (freq[s[right]] > 0)
+                required--;
+
+            // Decrease frequency (include in window)
             freq[s[right]]--;
 
-            if (freq[s[right]] >= 0) required--;
+            // When window is valid
+            while (required == 0) {
 
-            while (required == 0){
-                
-                if (min_len > right-left+1){
+                // Update minimum window
+                if (right - left + 1 < min_len) {
+                    min_len = right - left + 1;
                     start = left;
-                    min_len = right-left+1;
                 }
 
-                freq[s[left]]+=1;
-                if (freq[s[left]] > 0) required+=1 ;
-                left+=1;
-                
+                // Remove left character from window
+                freq[s[left]]++;
+
+                // If removing makes it needed again
+                if (freq[s[left]] > 0)
+                    required++;
+
+                left++;
             }
         }
 
-        return (min_len == INT_MAX) ? "" :s.substr(start, min_len);
-
+        return (min_len == INT_MAX) ? "" : s.substr(start, min_len);
     }
 };
