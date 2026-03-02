e19fc0cf2498c532ac1cb3f994d4995914ab1317
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-03-02 12:27:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,47 +1,35 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
+        
+        int required = t.size();
+        int start , min_len = INT_MAX, left = 0;
+        vector<int> freq(130, 0);
 
-       
-        unordered_map <char, int> need , have;
 
-        for (int i=0; i<t.size(); i++) need[t[i]]+=1;
+        for (int i =0; i<t.size(); i++) freq[t[i]]+=1;
 
-        int need_cnt = need.size(), have_cnt = 0, start;
+        for (int right = 0; right<s.size(); right++){
+            
+            freq[s[right]]--;
 
-        int min_len = INT_MAX;
-        int left = 0;
+            if (freq[s[right]] >= 0) required--;
 
-        for (int right =0; right<s.size(); right++){
-            char c = s[right];
-
-            if (need.count(c)) have[c]+=1;
-
-            if ( need.count(c) && need[c] == have[c]) have_cnt +=1;
-
-    
-            while (have_cnt == need_cnt){
-
+            while (required == 0){
+                
                 if (min_len > right-left+1){
                     start = left;
                     min_len = right-left+1;
                 }
-                char k = s[left];
 
-                if (need.count(k) && need[k] >= have[k]) have_cnt-=1;
-
-                have[k]-=1;
-
-                if (have[k] == 0) have.erase(k);
+                freq[s[left]]+=1;
+                if (freq[s[left]] > 0) required+=1 ;
                 left+=1;
+                
             }
         }
 
-        if (min_len == INT_MAX) return "";
-
-        return s.substr(start , min_len);
-
-
+        return (min_len == INT_MAX) ? "" :s.substr(start, min_len);
 
     }
 };
