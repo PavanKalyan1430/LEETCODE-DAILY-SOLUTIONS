776bed493a5ef3548e805636f87f2c8f1ef2374d
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-03-03 21:48:34 +0530

    Time: 88 ms (40.79%), Space: 56.3 MB (21.22%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -1,41 +1,38 @@
-int count_subarrays(vector<int>& nums, int k){
+int fun(vector<int>&nums, int k){
 
-    int left = 0;
-    int result_count= 0;
+    int left = 0; 
+    int cnt = 0;
 
-    unordered_map <int, int> m;
+    unordered_map <int,int> m;
 
-    for (int right =0; right<nums.size(); right++){
-        m[nums[right]]++;
+    for (int right=0; right<nums.size(); right++){
+
+        m[nums[right]]+=1;
 
         while (m.size() > k){
+            m[nums[left]] -=1;
 
-            m[nums[left]]--;
-
-            if (m[nums[left]] == 0){
-                m.erase(nums[left]);
-            }
-            
-            left++;
+            if (m[nums[left]] == 0) m.erase(nums[left]);
+            left+=1;
         }
 
-        result_count +=  (right-left+1);
+        cnt += (right-left+1);
     }
 
-    return result_count;
-}
+    
 
-   
+    return cnt;
+
+}
 
 
 class Solution {
 public:
     int subarraysWithKDistinct(vector<int>& nums, int k) {
+        
+        int cnt_k = fun(nums , k);
+        int cnt_k_minus_1 = fun(nums, k-1);
 
-        int atmost_K = count_subarrays(nums, k);
-        int atmost_Kminus1 = count_subarrays(nums, k-1);
-
-        return  atmost_K -  atmost_Kminus1;
-
-    }
+        return cnt_k - cnt_k_minus_1;
+     }
 };
