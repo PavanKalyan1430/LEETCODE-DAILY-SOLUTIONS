74e380f8910770058ef1cea0a754e3ae433e5c90
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-03-03 21:15:03 +0530

    Time: 3 ms (77.83%), Space: 11.7 MB (37.24%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        
+
         vector<int> need(26, 0);
         vector<int> have(26, 0);
-        vector<int> result ;
+        vector<int> result;
 
-        for (char i : p) need[i-'a']+=1;
+        for (char c : p) need[c-'a']+=1;
 
         int left = 0;
 
-        for (int right = 0; right<s.size(); right++){
+        for (int right = 0 ;right<s.size(); right++){
 
-            have[s[right] - 'a']+=1;
+            int index = s[right]- 'a';
+            have[index]+=1;
 
-            if (right- left + 1 == p.size()){
-                if (need == have){
-                    result.push_back(left);
-                }
+            if (right-left+1 == p.size()){
+
+                if (have == need) result.push_back(left);
 
                 have[s[left] - 'a']-=1;
-                left +=1;
+                left+=1;
             }
         }
-
         return result;
 
     }
+
 };
