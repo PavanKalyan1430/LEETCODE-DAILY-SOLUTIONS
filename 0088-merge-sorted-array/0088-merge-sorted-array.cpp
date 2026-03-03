287d6e024d0eba9ff9247f135d15b815bf910c2a
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-03-03 17:42:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,70 +1,29 @@
 class Solution {
 public:
-    void merge(vector<int>& a, int m, vector<int>& b, int n) {
-        a.erase(a.begin()+m,a.end());
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         
-        vector <int> res;
-        
-        int left = 0;
-        int right =0;
-        
-        while(left<a.size() and right<b.size()){
-            
-            
-            if (a[left] <= b[right]){
-                
-              //  if (res.empty() || res.back() != a[left] ){
-                    
-                res.push_back(a[left]);
-                
-                 left+=1;
-                
+        int i = m -1;
+        int j = n -1;
+        int k = m + n- 1;
+
+        while (i >=0 && j>=0){
+            int a = nums1[i];
+            int b = nums2[j];
+
+            if (b > a){
+                nums1[k--] = b;
+                j-=1; 
             }
-            
-            else if (b[right] < a[left])  {
-                
-              //  if (res.empty() || res.back() != b[right] ){
-                    
-                res.push_back(b[right]);
-                
-                right+=1;
+
+            else{
+                nums1[k--] = a; 
+                i-=1; 
             }
-            
-            // else {
-                
-            //    // if (res.empty() || res.back() != a[left] ){
-            //         res.push_back(a[left]);
-            //         left+=1;
-            //         right+=1;
-                    
-                
-           // }
-            
         }
-        
-        while(left<a.size()){
-            
-            //if (res.empty() || res.back() != a[left]){
-            res.push_back(a[left]);
-            left+=1;
+
+        while (j>= 0){
+            nums1[k--] = nums2[j];
+            j-=1;
         }
-        
-        while(right<b.size()){
-           // if (res.empty() || res.back() != b[right]){ ;
-            res.push_back(b[right]);
-            right+=1;
-        }
-        
-        a= res;
-        
     }
 };
-
-
-    //     int h=0,i=0,j=0;
-    //     vector <int> res;
-    //     nums1.erase(nums1.begin()+m,nums1.end());
-    //    nums1.insert(nums1.end(),nums2.begin(),nums2.end());
-    //    sort(nums1.begin(),nums1.end());}};
-       
-      
