8ed3de02b2284da432409aac715dd29df22bc4f9
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-03-07 17:41:32 +0530

    Time: 4 ms (75.21%), Space: 9.8 MB (93.14%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
@@ -0,0 +1,55 @@
+/**
+ * // This is the MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class MountainArray {
+ *   public:
+ *     int get(int index);
+ *     int length();
+ * };
+ */
+
+class Solution {
+public:
+    int findInMountainArray(int target, MountainArray &mountainArr) {
+        
+        int n = mountainArr.length();
+
+        int left = 0;
+        int right = n-1;
+
+        
+
+        while (left < right){
+            int mid = (left+right)/2;
+
+            if (mountainArr.get(mid) < mountainArr.get(mid+1)) left = mid+1;
+            else right = mid;
+        }
+
+        int peak = left;
+
+         left = 0;
+         right = peak;
+
+        while(left <= right){
+            int mid = ( left + right ) / 2;
+
+            if (mountainArr.get(mid) == target) return mid;
+            else if (mountainArr.get(mid) < target) left = mid+1;
+            else right = mid-1;
+        }
+
+        left = peak; right = n-1;
+
+        while(left <= right){
+            int mid = ( left + right ) / 2;
+
+            if (mountainArr.get(mid) == target) return mid;
+            else if (mountainArr.get(mid) > target) left = mid+1;
+            else right = mid-1;
+        }
+        return -1;
+
+
+    }
+};
