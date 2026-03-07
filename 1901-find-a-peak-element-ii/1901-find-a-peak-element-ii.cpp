3b9e354720144c66167db50402e87ecb93609a60
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-03-07 22:05:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -0,0 +1,49 @@
+int get_row(vector<vector<int>>&mat, int c){
+
+    int maxi = 0;
+    int index = 0;
+
+    for (int i=0; i<mat.size(); i++){
+        if (mat[i][c] > maxi){
+            maxi = mat[i][c];
+            index = i;
+        }
+    }
+
+    //cout<<index;
+    return index;
+
+}
+
+class Solution {
+public:
+    vector<int> findPeakGrid(vector<vector<int>>& mat) {
+        
+        int rows = mat.size();
+        int cols = mat[0].size();
+
+        int left = 0;
+        int right = cols-1;
+
+        while (left <= right) {
+
+            int mid = (left+right)/2;
+            int c  = mid;
+            int r = get_row(mat,c);
+
+            int ll = (c == 0) ? -1 : mat[r][c-1];
+            int rr = (c == cols-1) ? -1 : mat[r][c+1];
+
+
+            cout<<mid<<endl;
+            cout<<ll<<"   "<<rr<<endl;
+
+            if ( mat[r][c] > ll && mat[r][c] > rr ) return {r,c};
+            else if (mat[r][c] < mat[r][rr]) left = mid+1;
+            else right = mid-1; 
+            
+        }
+        return {-1,-1};
+        
+    }
+};
