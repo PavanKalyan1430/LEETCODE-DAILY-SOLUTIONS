a243f2c60b89e66ea3c24236a96071a42782c027
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-03-07 17:57:07 +0530

    Time: 0 ms (100%), Space: 26.3 MB (34.35%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,31 +1,43 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int left = 1;
-        int right = nums.size()-2;
-        int size=nums.size()-1;
+        
 
-        if (nums.size()==1) return nums[0];
-        if (nums[0] != nums[1]) return nums[0];
-        if (nums[size] != nums[size-1]) return nums[size];
 
-        while( left <= right){
-            cout<< left <<" "<< right<<endl;
-            int mid = ( left + right ) / 2;
-            cout<<"mid "<<mid<<endl;
-            if (nums[mid] != nums[mid-1] and nums[mid]!= nums[mid+1]) return nums[mid];
-
-            else if (nums[mid] == nums[mid-1]){
-                if ( (mid-1) % 2 != 0) right  = mid-1;
-                else left = mid+1; 
-            }
-            else {
-                if (mid % 2 != 0) right = mid-1;
-                else left = mid+1;
-            }
+         int left = 0, right = nums.size() - 1;
+        while(left < right){
+            int mid = (left + right)/2;
+            if((mid % 2 == 0 && nums[mid] == nums[mid + 1]) || (mid % 2 == 1 && nums[mid] == nums[mid - 1]))
+                left = mid + 1;
+            else
+                right = mid;
         }
+        
+        return nums[left];
 
-        return -1;
 
+
+
+
+        // int left = 0;
+        // int right = nums.size()-1;
+
+        // int mid ;
+
+        // if (nums.size()== 1) return nums[0];
+
+        
+        // while (left <right){
+        //     mid = left + (right - left)/2;
+
+        //     if (nums[mid] == nums[mid-1] && (mid-1) % 2 == 0) left = mid+1;
+
+        //     else if (nums[mid] == nums[mid+1] && mid % 2 == 0) left = mid+1;
+
+        //     else right = mid;
+
+        // }
+
+        // return nums[left];
     }
 };
