b285b3e4a4e43b5c81bcf339f238e8205d9de331
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-03-09 22:26:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,42 +1,39 @@
-void fun(vector<string>&result, int n , int open , int close, string& temp){
+void fun(vector<string>&result, string &temp, int n , int open, int close){
 
-    if (2*n == temp.size()){
+    if ( (2 * n ) == temp.size()){
         result.push_back(temp);
         return;
     }
 
     if (open < n){
         temp += '(';
-        fun(result, n, open+1, close, temp);
+        fun(result, temp, n, open+1, close);
         temp.pop_back();
     }
 
-     if (close < open){
+    if (close < open){
         temp += ')';
-        fun(result, n, open, close+1, temp);
+        fun(result, temp, n, open, close+1);
         temp.pop_back();
     }
 
     return;
 
-    
-
-
-    
-    
-
-
 }
+
 
 class Solution {
 public:
     vector<string> generateParenthesis(int n) {
         
         vector<string> result;
-
-        int open = 0, close = 0;
         string temp = "";
-        fun(result ,n ,open , close, temp);
+
+        int open = 0;
+        int close = 0;
+
+        fun(result, temp, n, open , close);
+
         return result;
     }
 };
