443d5eb2731eaf9d8bd73c9ea9b3a614fb8f0609
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-03-10 15:58:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -1,27 +1,24 @@
-void HelperFunction(TreeNode* root, int level , vector<int>&res){
-    
-    if (!root) return ;
-    
-    if (level == res.size()) res.push_back(root->val);
-    
-    HelperFunction(root->right, level+1, res);
-    
-    HelperFunction(root->left, level+1, res);
-    
+void dfs(TreeNode* root, int level , vector<int>& result){
+
+    if (!root) return;
+
+    if (level == result.size()) result.push_back(root->val);
+    dfs(root->right, level+1, result);
+    dfs(root->left, level+1, result);
+
+    return;
 }
+
+
+
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        vector<int> res ;
+        
         int level = 0;
-        
-        HelperFunction(root,level, res);
-        
-        return res;
+        vector<int> result;
+
+        dfs(root, level, result);
+        return result;
     }
 };
-
-
-
-
-
