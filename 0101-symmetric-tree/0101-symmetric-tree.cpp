376ad225e7050539acdf7d5945757512b5e208ac
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-03-10 14:51:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -1,31 +1,16 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+bool DFS(TreeNode* root1, TreeNode* root2){
+    if (!root1 && !root2) return true;
+    else if (!root1 || !root2) return false;
 
- bool HelperFunction(TreeNode* root1, TreeNode* root2){
+    else if (root1->val != root2->val) return false;
 
-    if (root1 == NULL and root2 == NULL) return true;
-
-    if (root1 == NULL or root2 == NULL) return false;
-
-    if (root1->val != root2->val)  return false;
-
-    return HelperFunction(root1->left, root2->right) && HelperFunction(root1->right, root2->left);
-
-
- }
+    bool left = DFS(root1->left, root2->right);
+    bool right = DFS(root1->right, root2->left);
+    return left && right;
+}
 class Solution {
 public:
-    bool isSymmetric(TreeNode* root) {
-        
-        return HelperFunction(root, root);
+    bool isSymmetric(TreeNode* root) {   
+        return DFS(root, root);
     }
 };
