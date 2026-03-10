eca55ac2c88c0aaf99256eb05b25c876a2685155
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-03-10 15:09:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -4,15 +4,15 @@ public:
     bool hasPathSum(TreeNode* root, int target) {
         if (!root) return false;
 
+        target -= root->val;
 
         if (!root->left && !root->right){
-            target -= root->val;
             if (target == 0) return true;
             else return false;
         }
 
-        bool left = hasPathSum(root->left, target-root->val);
-        bool right = hasPathSum(root->right, target-root->val);
+        bool left = hasPathSum(root->left, target);
+        bool right = hasPathSum(root->right, target);
         return left || right;
 
     }
