8200d4f965e21ce402f577aeb6b58a19402f5f39
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-03-10 14:45:24 +0530

    Time: 0 ms (100%), Space: 12.9 MB (46.76%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -9,28 +9,17 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
- bool HelperFunction(TreeNode* root1, TreeNode* root2){
-
-    if (!root2 and !root1)  return true;
-
-    if (root1 ==NULL or root2==NULL) return false;
-
-
-    if (root1->val != root2->val)  return false;
-
-    
-
-    return HelperFunction(root1->left, root2->left) and HelperFunction(root1->right, root2->right);
-
-    
-
-
- }
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p && !q) return true;
 
-        return HelperFunction(p, q);
+        else if (!p || !q) return false;
+
+        else if (p->val != q->val) return false;
+
+        bool left = isSameTree(p->left, q->left);
+        bool right = isSameTree(p->right, q->right);
+        return left && right;
     }
 };
