5f0ace52a7f7abd54c4290a87a197fdb768b93ff
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-03-11 09:24:59 +0530

    Time: 0 ms (100%), Space: 11.9 MB (39.27%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -1,43 +1,32 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+void dfs(TreeNode* root, int &sum, int temp){
 
-void HelperFunction(TreeNode* root, int ans, int &result){
+    if ( !root) return;
 
-    if (!root) return ;
+    temp = temp*10 + root->val;
 
-     ans = ans*10 + root->val;
+    if (!root->left &&  !root->right){
+        sum += temp;
+    }
 
-    if (!root->left and !root->right) result += ans;
+    dfs(root->left, sum, temp );
+    dfs(root->right, sum, temp );
 
-    HelperFunction(root->left, ans, result);
-    HelperFunction(root->right, ans, result);
+    return;
 
 
 }
+
+
 
 class Solution {
 public:
     int sumNumbers(TreeNode* root) {
         
-        int ans = 0;
-        
-        int result = 0;
-        HelperFunction(root, ans, result);
+        int sum  = 0;
+        int temp = 0;
 
-        
+        dfs(root, sum, temp);
 
-        return result;
-
-
-
+        return sum;
     }
 };
