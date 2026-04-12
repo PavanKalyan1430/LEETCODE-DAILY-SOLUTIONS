745e8d5096f4ec14a730d5612c40bde6df1e921c
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-04-12 12:27:52 +0530

    Time: 15 ms (11.11%), Space: 40.6 MB (0%) - LeetHub

---
+++ 3895-count-digit-appearances/3895-count-digit-appearances.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countDigitOccurrences(vector<int>& nums, int digit) {
+        
+        string s = "";
+        int cnt = 0;
+
+        for (int i=0; i<nums.size(); i++){
+            s+= to_string(nums[i]);
+        }
+
+        cout<<s<<" ";
+
+        for (int i=0; i<s.size(); i++){
+            cnt += ( (s[i] - '0') == digit)  ? 1 : 0;
+        }
+
+        return cnt;
+
+    }
+};
