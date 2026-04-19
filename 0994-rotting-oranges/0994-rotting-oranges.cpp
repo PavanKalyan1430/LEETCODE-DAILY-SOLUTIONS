91750379eadf2ca233d3e33ee1d08fa472cbd423
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-04-19 10:33:45 +0530

    Time: 0 ms (100%), Space: 16.7 MB (85.93%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,69 +1,60 @@
 class Solution {
 public:
-    int orangesRotting(vector<vector<int>>& grid) {
-         queue<pair<int, int>> q;
-        int min = 0;
+    int orangesRotting(vector<vector<int>>& nums) {
         
-        
-        int r = grid.size();
-        int c = grid[0].size();
-        
-        
+        queue<pair<int,int>> q;
+
         int fresh = 0;
-        int rotten  = 0;
-        
-        
-        for (int i=0; i<r; i++){
-            for (int j = 0; j<c; j++){
+        int min = 0;
+
+        int rows = nums.size();
+        int cols = nums[0].size();
+
+        for (int i=0; i<rows; i++){
+            for (int j =0; j<cols ; j++){
                 
-                if (grid[i][j] == 2){
+                if (nums[i][j] == 1) fresh+=1;
+                else if (nums[i][j] == 2){
                     q.push({i,j});
                 }
-                
-                else if (grid[i][j] == 1) fresh+=1;
             }
         }
-        
-        if (fresh == 0) return 0;
-        
-        vector<int> dx = {1,0,-1,0};
-        vector<int> dy = {0,-1,0,1};
-        
-        while(!q.empty()){
-            
-         
-            
-            min += 1;
-            int siz = q.size();
-            
-            
-            for (int p = 0; p<siz; p++){
-                
-                pair <int ,int> node = q.front();q.pop();
-                
-                int x = node.first;
-                int y = node.second;
-                
+
+        if (fresh  == 0) return 0;
+
+        vector<int> dx = {0,-1,0,1};
+        vector<int> dy = {1,0,-1,0};
+
+        while (!q.empty()){
+
+            min +=1;
+
+            int sz = q.size();
+            for (int i=0; i<sz; i++){
+
+                auto [x,y] = q.front();  q.pop();
+
                 for (int k =0; k<4; k++){
-                    int i = x + dx[k];
-                    int j = y + dy[k];
-                    
-                    if (i < r && j < c && i>= 0 && j>= 0 && grid[i][j] == 1){
+
+                    int m = x + dx[k];
+                    int n = y + dy[k];
+
+                    if (m<rows && n<cols && m>=0 && n>=0 && nums[m][n] == 1){
+                        nums[m][n] = 2;
                         fresh -=1;
-                        grid[i][j] = 2;
-                        q.push({i,j});
-                        
-                        
+                        q.push({m,n});
+
                     }
-                    
-                if (fresh == 0) return min;
+
                 }
+
+                if (fresh == 0) return min;
+
+                
             }
-            
-            
+
         }
-        
-        return -1 ;
+
+        return -1;
     }
 };
-
