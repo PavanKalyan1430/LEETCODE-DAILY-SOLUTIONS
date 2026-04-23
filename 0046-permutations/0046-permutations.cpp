b566874322c97cfab0dd6b64fd1344c961082a28
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-04-23 23:28:01 +0530

    Time: 0 ms (100%), Space: 10.6 MB (68.27%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,14 +1,18 @@
-void fun(vector<int>&nums, vector<vector<int>>&result, int idx){
 
-    if (idx == nums.size()) result.push_back(nums);
+void fun(vector<int>&nums, vector<vector<int>> &result, vector<int>&temp,int ind ){
 
-
-    for (int i = idx; i<nums.size(); i++){
-        swap(nums[i], nums[idx]);
-        fun(nums, result, idx+1);
-        swap(nums[i], nums[idx]);
+    if (ind == nums.size()){
+        result.push_back(nums);
+        return;
     }
-    
+
+    for (int i=ind; i<nums.size(); i++){
+        swap(nums[i], nums[ind]);
+        fun(nums, result, temp, ind+1);
+        swap(nums[i], nums[ind]);
+    }
+
+    return;
 
 }
 
@@ -18,8 +22,11 @@ public:
     vector<vector<int>> permute(vector<int>& nums) {
         
         vector<vector<int>> result;
+        vector<int> temp;
 
-        fun(nums, result, 0);
+        int ind  =0;
+
+        fun(nums, result, temp, ind);
 
         return result;
     }
