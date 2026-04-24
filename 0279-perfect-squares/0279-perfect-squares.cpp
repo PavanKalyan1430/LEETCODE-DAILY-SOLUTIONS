9246c18605717b7ab8bda303ef966fbd8bd3a3a8
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-04-24 22:44:45 +0530

    Time: 210 ms (13.92%), Space: 162.7 MB (6.08%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        
+        int i = 1;
+
+        vector<int> nums;
+
+        while ( (i*i) <= n){
+            nums.push_back(i*i);
+            i+=1;
+        }
+
+        int k = nums.size();
+
+        for (int i : nums) cout<<i<<" ";
+
+        vector<vector<int>> dp(k+1, vector<int>(n+1, INT_MAX-1));
+
+        for (int i=0; i<=k ;i++) dp[i][0] = 0;
+
+        for (int i =1; i<=k;  i++){
+            for (int j = 1; j<=n; j++){
+                if (j >= nums[i-1]){
+                    dp[i][j] = min(dp[i-1][j] , 1+dp[i][j-nums[i-1]]);
+                }
+                else dp[i][j] = dp[i-1][j];
+            }
+        }
+
+        return dp.back().back();
+    }
+};
