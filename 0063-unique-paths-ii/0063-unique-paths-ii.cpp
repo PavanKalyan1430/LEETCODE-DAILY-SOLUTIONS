7701f9bbc55b73e2da6127b928351e25f8fed170
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-04-24 22:58:18 +0530

    Time: 0 ms (100%), Space: 11.8 MB (68.97%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,38 +1,43 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& nums) {
+    int uniquePathsWithObstacles(vector<vector<int>>& nums){        
         
-
-        if (nums[0][0] == 1 || nums.back().back() == 1) return 0;
-
         int rows = nums.size();
         int cols = nums[0].size();
 
+        if (nums[0][0] == 1 || nums[rows-1][cols-1] == 1) return 0;
+
         vector<vector<int>> dp(rows, vector<int>(cols, 0));
 
-
         for (int i=0; i<rows; i++){
-            if (nums[i][0] != 1) dp[i][0] = 1;
+            if (nums[i][0] == 0) dp[i][0] = 1;
             else break;
         }
 
-        for (int i=0; i<cols; i++){
-            if (nums[0][i] != 1) dp[0][i] = 1;
+         for (int i=0; i<cols; i++){
+            if (nums[0][i] == 0) dp[0][i] = 1;
             else break;
         }
 
         for (int i=1; i<rows; i++){
-            for (int j = 1; j<cols; j++){
+            for (int j = 1; j<cols ; j++){
+                if (nums[i][j] == 0){
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
                 
-                if (nums[i][j] == 1) continue;
-
-                dp[i][j] = dp[i][j-1] + dp[i-1][j];
             }
         }
 
-        return dp[rows-1][cols-1];
+        // for (auto i : dp){
+        //     for (auto j : i) cout<<j<<" ";
+        //     cout<<endl;
+        // }
 
+        // cout<<endl;
 
+       // cout<<dp.back().back();
+
+        return dp.back().back();
 
     }
 };
