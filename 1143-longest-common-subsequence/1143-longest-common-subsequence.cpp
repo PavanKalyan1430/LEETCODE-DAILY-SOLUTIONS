a5343ff6fb5894b4ac34736bb0f20e3018a7b104
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-04-25 00:01:40 +0530

    Time: 26 ms (64.53%), Space: 27.3 MB (75.69%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,35 +1,21 @@
-int fun (string &s1 , string&s2 , int i , int j, vector<vector<int>>&dp){
-
-    if (i <0 || j<0) return 0;
-
-    if (dp[i][j] != -1) return dp[i][j];
-
-    if (s1[i] == s2[j]){
-        return dp[i][j] = 1 + fun(s1, s2, i-1, j-1, dp);
-    }
-
-    else {
-        int pick_1st_string = fun(s1, s2, i, j-1, dp);
-        int pick_2nd_string = fun(s1, s2, i-1, j, dp);
-
-        return dp[i][j] = max(pick_1st_string , pick_2nd_string);
-    }
-
-
-}
-
 class Solution {
 public:
-    int longestCommonSubsequence(string str1, string str2) {
+    int longestCommonSubsequence(string p, string q) {
         
+        int n = p.size();
+        int m = q.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
 
-        int n = str1.size();
-        int m = str2.size();
+        for (int i=1; i<=n; i++){
+            for (int j =1; j<=m; j++){
+                if (  p[i-1] == q[j-1] ){
+                    dp[i][j] = 1 + dp[i-1][j-1] ;
+                }
+                else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
 
-        vector<vector<int>> dp(n, vector<int>(m+1, -1));
-
-        return fun(str1 , str2 , n-1, m-1, dp);
-
+        return dp.back().back();
 
     }
 };
