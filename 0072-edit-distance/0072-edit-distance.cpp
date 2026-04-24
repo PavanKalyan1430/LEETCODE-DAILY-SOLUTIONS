2675c86de60b64aa6bda14d50c2c4d1518110d10
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-04-25 00:22:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -5,10 +5,11 @@ public:
         int n = word1.size();
         int m = word2.size();
 
+
         vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
 
-        for (int i= 1; i<=m ; i++) dp[0][i] = i;
-        for (int i=1 ; i<=n; i++) dp[i][0] = i;
+        for (int i=1; i<=n; i++) dp[i][0] = i;
+        for (int i=1; i<=m; i++) dp[0][i] = i;
 
         for (int i=1; i<=n; i++){
             for (int j = 1; j<=m; j++){
@@ -17,12 +18,11 @@ public:
                     dp[i][j] = dp[i-1][j-1];
                 }
 
-                else {
-                    dp[i][j] =  1+ min(dp[i-1][j] , min(dp[i-1][j-1], dp[i][j-1]));
-                }
+                else dp[i][j] = 1 + min({ dp[i-1][j-1], dp[i][j-1], dp[i-1][j]});
             }
         }
 
-        return dp[n][m];
+        return dp.back().back();
+
     }
 };
