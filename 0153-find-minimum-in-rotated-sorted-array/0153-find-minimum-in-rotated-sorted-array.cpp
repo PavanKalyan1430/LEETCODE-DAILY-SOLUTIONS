7ebe32601c3ffab52d3108c7f73b4f96b76d06c2
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-06-22 12:28:03 +0530

    Time: 0 ms (100%), Space: 14 MB (77.78%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -4,21 +4,15 @@ public:
         
         int left = 0;
         int right = nums.size()-1;
-        int mid;
 
         while (left < right){
-            
-            mid = ( left + right ) / 2;
-
-            if (nums[mid] >= nums[right] ){
-                left =  mid+1;
-            }
-
+            int mid = (left + right )/2;
+            if (nums[mid] > nums[right]) left = mid+1;
             else right = mid;
-
 
         }
 
         return nums[left];
+
     }
 };
