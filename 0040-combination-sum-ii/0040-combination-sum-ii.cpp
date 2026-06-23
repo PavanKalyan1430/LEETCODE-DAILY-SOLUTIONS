bbb1344d55cbc9ad14ef0c8fe6b85afa445d612e
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-06-23 11:47:11 +0530

    Time: 3 ms (37.67%), Space: 14 MB (61.98%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,54 +1,42 @@
-void fun(vector<int>&nums, vector<vector<int>>&result, vector<int>&temp, int target, int start, int &n ){
+void fun(vector<int>&nums, vector<int>&temp, vector<vector<int>>&result, int target, int idx){
+
 
     
-        if (target == 0){
-            result.push_back(temp);
-            return;
-        }
-    
+    if (target == 0){
+        result.push_back(temp);
+        return;
+    }
 
-
-
-    for (int i=start; i<n; i++){
-
-        if (i != start and nums[i] == nums[i-1]) continue;
+    for (int i=idx ; i<nums.size(); i++) {
 
         if (nums[i] > target) break;
-        
-        
-            temp.push_back(nums[i]);
-           
+        if ( i != idx && nums[i] == nums[i-1]) continue;
 
-            fun(nums, result, temp, target - nums[i] , i+1, n);
-
-            temp.pop_back();
-           
-        
+        temp.push_back(nums[i]);
+        fun(nums, temp, result, target-nums[i], i+1);
+        temp.pop_back();
     }
 
     return;
 
 
 
-
 }
+
 
 
 class Solution {
 public:
     vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
         
-        vector<vector<int>> result;
-        vector<int> temp;
-        int i = 0;
-
-        int  n = nums.size();
-
         sort(nums.begin(), nums.end());
 
-        fun(nums, result, temp, target, i, n);
+
+        vector<int>temp;
+        vector<vector<int>> result;
+
+        fun(nums, temp, result, target , 0);
 
         return result;
-
     }
 };
