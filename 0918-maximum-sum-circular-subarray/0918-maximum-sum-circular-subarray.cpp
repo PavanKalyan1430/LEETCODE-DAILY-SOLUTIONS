3ad805a6f2e9052647870fe0fc335ea0ec359125
B. Pavan Kalyan Reddy <152968280+PavanKalyan1430@users.noreply.github.com>
2026-07-05 23:43:56 +0530

    Time: 0 ms (100%), Space: 43.5 MB (97.66%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
@@ -2,43 +2,35 @@ class Solution {
 public:
     int maxSubarraySumCircular(vector<int>& nums) {
         
-        int total = 0;
-        total = accumulate(nums.begin(), nums.end(), 0);
 
-        int max_subarray_sum = nums[0] ;
-        int max_result = nums[0];
 
-        int min_subarray_sum = nums[0];
-        int min_result = nums[0];
+        int min_sum = nums[0], total = nums[0];
+        int max_sum =  nums[0];
+
+        int pos = nums[0];
+        int neg = nums[0];
 
         for (int i=1; i<nums.size(); i++){
 
-            // fiding the minimum subarray sum
-            if (min_subarray_sum > 0) min_subarray_sum = 0;
+            if (pos < 0) pos = 0;
+            pos += nums[i];
+            max_sum = max(max_sum , pos);
 
-            min_subarray_sum += nums[i];
+            if (neg > 0) neg = 0;
+            neg += nums[i];
+            min_sum = min(min_sum , neg);
 
-            min_result = min(min_result, min_subarray_sum);
+            total += nums[i];
 
-
-            // finding the maximum subarray sum
-            if (max_subarray_sum < 0) max_subarray_sum = 0;
-
-            max_subarray_sum += nums[i];
-
-            max_result = max(max_result, max_subarray_sum);
         }
 
-        // max_subarray_sum is negative it means it is a single element whcih       
-        //is negative and also the largest among all negative number (ex: -1 , -10, 100) :  [ans is -1]
-        if (max_result < 0){
-            return max_result;
-        }
+        // if all are negative elements {-1,-10,100} -1 will be ans
+        if (max_sum < 0) return max_sum;
 
-        // now we have two options either the max_result can be 
-        //the maximum or the total - minimum subarray
+        // now we have two cases , total , toatl - min_sum
 
-       return max(max_result, total- min_result);
-    
+        return max(max_sum, total-min_sum);
+
+
     }
 };
