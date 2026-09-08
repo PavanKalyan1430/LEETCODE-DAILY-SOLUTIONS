9b0a57781e3300ba545be559d0f0538ec481a573
B. Pavan Kalyan Reddy <pk1698629@gmail.com>
2026-09-08 19:20:20 +0530

    Time: 9 ms (69.5%), Space: 13.9 MB (85.61%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -2,40 +2,49 @@ class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
         
-        int n = nums.size();
-        int mindiff  = INT_MAX, ans; 
+        // {-2,-1, 1,2,3}
+
+        // target = 3      {2,1,-1} => 2    {-2,3,2} => 3   {-2,-1,1}=> -2   
+
+       // difference  =  abs(sum - target)
+
 
         sort(nums.begin(), nums.end());
+        int min_diff = INT_MAX;
+        int result = 0;
+        int sz = nums.size();
 
-        for (int i=0 ; i<nums.size(); i++){
 
-            if (i!=0 && nums[i] == nums[i-1]) continue;
+        for (int i=0; i<sz-2; i++){
+
+            if (i != 0 && nums[i] == nums[i-1]) continue;
 
             int left = i;
-            int mid =  i+1;
-            int right = n-1;
+            int mid = i+1;
+            int right = sz-1;
 
             while (mid < right){
                 int sum = nums[left] + nums[mid] + nums[right];
-                int diff = abs (target - sum);
 
-                if (diff < mindiff){
-                    mindiff = diff;
-                   // cout<<nums[left] << "  "<<nums[mid]<< "   "<<nums[right]<<endl;
-                    ans = sum;
+                if (sum == target) return sum;
+
+                else if (abs(sum - target) <min_diff){
+
+                    min_diff = abs(sum - target);
+                    result = sum;
+
                 }
 
-                if (sum == target) return target;
-
-                else if (sum < target) mid+=1;
-
+                if (sum < target) mid+=1;
                 else right-=1;
-                
+
+                      
             }
 
-            
-        }
+       }
 
-        return ans;
+       return result;
+  
+
     }
 };
